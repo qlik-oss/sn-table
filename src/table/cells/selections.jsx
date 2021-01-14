fd898f2b02c9cde2b80d09284d77416b2e4a3299
Max Gefvert <max.gefvert@qlik.com>
2021-01-14 08:50:29 +0100

    fix: remove comment

--- src/table/cells/selections.jsx
+++ src/table/cells/selections.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import handleSelect from '../handle-select';
 
 export default function withSelections(CellComponent) {
   const HOC = (props) => {
