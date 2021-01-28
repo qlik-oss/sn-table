2167b2e32d894b67563e7aeffcc64d133a89ec93
Max Gefvert <max.gefvert@qlik.com>
2021-01-28 11:00:35 +0100

    fix: revert exports named

--- rollup.config.js
+++ rollup.config.js
@@ -17,7 +17,7 @@ export default {
     file: path.resolve(__dirname, 'dist', 'sn-table.js'),
     name: 'A Table',
     format: 'umd',
-    exports: 'named',
+    exports: 'default',
     sourcemap: true,
     globals: {
       '@nebula.js/stardust': 'stardust',
