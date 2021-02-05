36ec121308889193d95b2114f6c84f7db1475a62
Max Gefvert <max.gefvert@qlik.com>
2021-02-05 16:09:36 +0100

    fix: add file

---
+++ src/__test__/event-listeners.spec.js
@@ -0,0 +1,83 @@
+import addEventListeners from '../event-listeners';
+
+describe('event-listeners', () => {
+  describe('addEventListeners', () => {
+    let alreadyActive = true;
+    const onFunctions = {};
+    let el;
+    let selectionsApi;
+    let remover;
+
+    beforeEach(() => {
+      el = {
+        addEventListener: (type, onFn) => {
+          onFunctions[type] = onFn;
+        },
+        removeEventListener: sinon.spy(),
+      };
+      selectionsApi = {
+        isActive: () => alreadyActive,
+        confirm: sinon.spy(),
+      };
+
+      remover = addEventListeners(el, selectionsApi);
+    });
+
+    it('should add eventlisteners for mousedown and mouseup', () => {
+      expect(onFunctions.mousedown).to.be.a('function');
+      expect(onFunctions.mouseup).to.be.a('function');
+    });
+
+    it('should return function that removes event listeners', () => {
+      remover();
+      expect(el.removeEventListener).to.have.been.calledWith('mousedown');
+      expect(el.removeEventListener).to.have.been.calledWith('mouseup');
+    });
+
+    describe('onMouseDown', () => {
+      let e;
+
+      beforeEach(() => {
+        e = {
+          target: {
+            clientWidth: 1000,
+            className: 'excluded',
+          },
+          offsetX: 500,
+          stopPropagation: sinon.spy(),
+        };
+      });
+
+      it('should call stopPropagation and confirm when selections are active and clicking on non-selectable element', () => {
+        onFunctions.mousedown(e);
+        onFunctions.mouseup(e);
+        expect(e.stopPropagation).to.have.been.calledOnce;
+        expect(selectionsApi.confirm).to.have.been.calledOnce;
+      });
+      it('should not call stopPropagation nor confirm when selections are not active', () => {
+        alreadyActive = false;
+
+        onFunctions.mousedown(e);
+        onFunctions.mouseup(e);
+        expect(e.stopPropagation).to.not.have.been.called;
+        expect(selectionsApi.confirm).to.not.have.been.called;
+      });
+      it('should not call stopPropagation nor confirm when clicking selectable cell', () => {
+        e.target.classes = 'possible';
+
+        onFunctions.mousedown(e);
+        onFunctions.mouseup(e);
+        expect(e.stopPropagation).to.not.have.been.called;
+        expect(selectionsApi.confirm).to.not.have.been.called;
+      });
+      it('should not call stopPropagation nor confirm when clicking on scrollbar (scrollbarDown = true)', () => {
+        e.offsetX = 1000;
+
+        onFunctions.mousedown(e);
+        onFunctions.mouseup(e);
+        expect(e.stopPropagation).to.not.have.been.called;
+        expect(selectionsApi.confirm).to.not.have.been.called;
+      });
+    });
+  });
+});
---
+++ src/event-listeners.js
@@ -0,0 +1,25 @@
+export default function addEventListeners(el, selectionsAPI) {
+  let scrollbarDown = false;
+
+  const onMouseDown = (e) => {
+    scrollbarDown = e.offsetX >= e.target.clientWidth;
+  };
+
+  const onMouseUp = (e) => {
+    const classes = e.target.className;
+    const isSelectableCell = classes.includes?.('selected') || classes.includes?.('possible');
+    if (selectionsAPI.isActive() && !isSelectableCell && !scrollbarDown) {
+      e.stopPropagation();
+      selectionsAPI.confirm();
+    }
+    scrollbarDown = false;
+  };
+
+  el.addEventListener('mousedown', onMouseDown, true);
+  el.addEventListener('mouseup', onMouseUp, true);
+
+  return () => {
+    el.removeEventListener('mousedown', onMouseDown, true);
+    el.removeEventListener('mouseup', onMouseUp, true);
+  };
+}
