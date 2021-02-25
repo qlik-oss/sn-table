df22fd82c636930000f3ee0090f7f8652ffd3b2a
Max Gefvert <max.gefvert@qlik.com>
2021-02-25 10:13:27 +0100

    fix: only select on left click

--- src/table/cells/__tests__/selections.spec.jsx
+++ src/table/cells/__tests__/selections.spec.jsx
@@ -13,6 +13,7 @@ describe('withSelections', async () => {
   let cell;
   let selState;
   let selDispatch;
+  let evt;
 
   beforeEach(() => {
     HOC = withSelections.default((props) => <div {...props}>{props.cell.value}</div>);
@@ -27,6 +28,7 @@ describe('withSelections', async () => {
       colIdx: -1,
     };
     selDispatch = () => {};
+    evt = { button: 0 };
   });
 
   afterEach(() => {
@@ -41,16 +43,24 @@ describe('withSelections', async () => {
   });
   it('should call selectCell on mouseUp', () => {
     const { queryByText } = render(<HOC cell={cell} selState={selState} selDispatch={selDispatch} />);
-    fireEvent.mouseUp(queryByText(cell.value));
+    fireEvent.mouseUp(queryByText(cell.value), evt);
 
     expect(selectionsUtils.selectCell).to.have.been.calledWith(cell, selState);
   });
-
   it('should not call selectCell on mouseUp when measure', () => {
     cell.isDim = false;
 
     const { queryByText } = render(<HOC cell={cell} selState={selState} selDispatch={selDispatch} />);
-    fireEvent.mouseUp(queryByText(cell.value));
+    fireEvent.mouseUp(queryByText(cell.value), evt);
+
+    expect(selectionsUtils.selectCell).to.not.have.been.called;
+  });
+
+  it('should not call selectCell on mouseUp when right button', () => {
+    evt.button = 2;
+
+    const { queryByText } = render(<HOC cell={cell} selState={selState} selDispatch={selDispatch} />);
+    fireEvent.mouseUp(queryByText(cell.value), evt);
 
     expect(selectionsUtils.selectCell).to.not.have.been.called;
   });
