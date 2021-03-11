a08d9d2078501509410c66b6be42f30a4c63db70
Max Gefvert <max.gefvert@gmail.com>
2021-03-11 13:53:02 +0100

    feat: column alignment (#119)

--- src/table/handle-data.js
+++ src/table/handle-data.js
@@ -18,7 +18,7 @@ export function getColumnInfo(layout, colIndex) {
       width: 200,
       label: info.qFallbackTitle,
       id: info.cId,
-      align: isDim ? 'left' : 'right',
+      align: !info.textAlign || info.textAlign.auto ? (isDim ? 'left' : 'right') : info.textAlign.align,
       stylingInfo: info.qAttrExprInfo.map((expr) => expr.id),
     }
   );
