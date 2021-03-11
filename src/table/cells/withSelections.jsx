1fd98d51da24dba537ff3d3ccdd4c81133802c99
Li Kang <kkllpaul7766@gmail.com>
2021-03-11 09:05:38 +0100

    remove keys navigation when attain focous by clicking a cell

--- src/table/cells/withSelections.jsx
+++ src/table/cells/withSelections.jsx
@@ -6,10 +6,10 @@ import { getSelectionStyle } from '../styling-utils';
 export default function withSelections(CellComponent) {
   const HOC = (props) => {
     const { cell, selState, selDispatch, styling } = props;
-    const handleMouseDown = (evt) => cell.isDim && evt.button === 0 && selectCell(cell, selState, selDispatch, evt);
+    const handleMouseUp = (evt) => cell.isDim && evt.button === 0 && selectCell(cell, selState, selDispatch, evt);
     const selectionStyling = useMemo(() => getSelectionStyle(styling, cell, selState), [cell, selState]);
 
-    return <CellComponent {...props} styling={selectionStyling} onMouseDown={handleMouseDown} />;
+    return <CellComponent {...props} styling={selectionStyling} onMouseUp={handleMouseUp} />;
   };
 
   HOC.propTypes = {
