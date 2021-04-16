f1442612dffe0d226365d06e0414b36f7db441d6
Max Gefvert <max.gefvert@qlik.com>
2021-04-16 15:28:06 +0200

    feat: extra properties

--- src/object-properties.js
+++ src/object-properties.js
@@ -44,6 +44,66 @@ const properties = {
     /** @type {boolean} */
     show: false,
   },
+  /** @type {Styling[]=} */
+  components: [],
 };
+
+/**
+ * Extends `NxDimension`, see Engine API: `NxDimension`.
+ * @typedef {object} DimensionProperties
+ * @extends NxDimension
+ * @property {QDefExtended=} qDef
+ */
+
+/**
+ * Extends `NxInlineDimensionDef`, see Engine API: `NxInlineDimensionDef`.
+ * @typedef {object} QDefExtended
+ * @extends NxInlineDimensionDef
+ * @property {TextAlign=} textAlign
+ */
+
+/**
+ * @typedef {object} TextAlign
+ * @extends NxInlineDimensionDef
+ * @property {boolean} auto
+ * @property {('left'|'center'|'right')=} align
+ */
+
+/**
+ * Extends `NxMeasure`, see Engine API: `NxMeasure`.
+ * @typedef {object} MeasureProperties
+ * @extends NxMeasure
+ * @property {qDef} coloring
+ * @property {MeasureAxis} measureAxis
+ * @property {ConditionalColoring} conditionalColoring
+ */
+
+/**
+ * @typedef {object} Styling
+ * @property {string} key
+ * @property {ContentStyling=} content
+ * @property {HeaderStyling=} header
+ */
+
+/**
+ * @typedef {object} ContentStyling
+ * @property {number=} fontSize
+ * @property {PaletteColor=} fontColor
+ * @property {boolean=} hoverEffect
+ * @property {PaletteColor=} hoverColor
+ * @property {PaletteColor=} hoverFontColor
+ */
+
+/**
+ * @typedef {object} HeaderStyling
+ * @property {number=} fontSize
+ * @property {PaletteColor=} fontColor
+ */
+
+/**
+ * @typedef {object} PaletteColor
+ * @property {number} index
+ * @property {string} color
+ */
 
 export default properties;
