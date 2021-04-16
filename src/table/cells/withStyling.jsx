5427cf1244179a5233fb4be218fd79484a40efae
Max Gefvert <max.gefvert@gmail.com>
2021-04-16 15:31:54 +0200

    feat: inline styling to minimize style tags (#168)

--- src/table/cells/withStyling.jsx
+++ src/table/cells/withStyling.jsx
@@ -1,35 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import { STYLING_DEFAULTS } from '../styling-utils';
-
-const useStyles = makeStyles({
-  cellBackground: (props) => ({
-    background: props.background,
-  }),
-  cellStyling: (props) => ({
-    color: props.fontColor,
-    backgroundColor: props.backgroundColor,
-    fontSize: props.fontSize,
-    padding: props.padding,
-    height: STYLING_DEFAULTS.HEIGHT,
-    lineHeight: STYLING_DEFAULTS.BODY_LINE_HEIGHT,
-    '&&:focus': {
-      boxShadow: STYLING_DEFAULTS.FOCUS_OUTLINE,
-    },
-  }),
-});
 
 export default function withStyling(CellComponent) {
   const HOC = (props) => {
     const { styling } = props;
-    const classes = useStyles(styling);
-    return (
-      <CellComponent
-        {...props}
-        className={`${classes.cellStyling} ${classes.cellBackground} ${styling.selectedCellClass} sn-table-cell`}
-      />
-    );
+    return <CellComponent {...props} className={`${styling.selectedCellClass} sn-table-cell`} style={styling} />;
   };
 
   HOC.propTypes = {
