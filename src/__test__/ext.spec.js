e56548e1f724189312178128d8bb73f653ea2cff
Max Gefvert <max.gefvert@gmail.com>
2021-04-22 07:45:34 +0200

    feat: adding properties to pp and documentation to object-properties (#176)

--- src/__test__/ext.spec.js
+++ src/__test__/ext.spec.js
@@ -1,3 +1,4 @@
+import { expect } from 'chai';
 import ext, { indexAdded, indexRemoved, min, getDescription } from '../ext';
 
 describe('ext', () => {
@@ -8,6 +9,143 @@ describe('ext', () => {
   };
   let array;
   let index;
+
+  describe('definition', () => {
+    const { definition } = ext(env);
+
+    describe('dimension/measure properties', () => {
+      const dimensionItems = definition.items.data.items.dimensions.items;
+
+      describe('visibilityCondition.isExpression', () => {
+        let val;
+
+        beforeEach(() => {
+          val = 'someExpresison';
+        });
+
+        it('should return true for valid expression', () => {
+          const isExpression = dimensionItems.visibilityCondition.isExpression(val);
+          expect(isExpression).to.be.true;
+        });
+
+        it('should return false for non string', () => {
+          val = 1;
+
+          const isExpression = dimensionItems.visibilityCondition.isExpression(val);
+          expect(isExpression).to.be.false;
+        });
+
+        it('should return false for string only containing white space', () => {
+          val = ' ';
+
+          const isExpression = dimensionItems.visibilityCondition.isExpression(val);
+          expect(isExpression).to.be.false;
+        });
+      });
+
+      describe('textAlign.show', () => {
+        let data;
+
+        beforeEach(() => {
+          data = {
+            qDef: {
+              textAlign: {
+                auto: false,
+              },
+            },
+          };
+        });
+
+        it('should return true when auto is false', () => {
+          const show = dimensionItems.textAlign.show(data);
+          expect(show).to.be.true;
+        });
+
+        it('should return false when auto is true', () => {
+          data.qDef.textAlign.auto = true;
+
+          const show = dimensionItems.textAlign.show(data);
+          expect(show).to.be.false;
+        });
+
+        it('should return false when textAlign is undefined', () => {
+          data.qDef.textAlign = undefined;
+          const show = dimensionItems.textAlign.show(data);
+          expect(show).to.be.false;
+        });
+      });
+    });
+
+    describe('styling', () => {
+      const stylingItems = definition.items.settings.items.presentation.items[0].items[0].items.chart.items;
+
+      describe('headerFontSize/fontSize.change', () => {
+        let data;
+
+        beforeEach(() => {
+          data = {
+            header: {
+              fontSize: 14,
+            },
+            content: {
+              fontSize: 14,
+            },
+          };
+        });
+
+        it('should not change size when in allowed value', () => {
+          stylingItems.headerFontSize.change(data);
+          stylingItems.fontSize.change(data);
+          expect(data.header.fontSize).to.equal(14);
+          expect(data.content.fontSize).to.equal(14);
+        });
+
+        it('should change size to 5 when less than 5', () => {
+          data.header.fontSize = 3;
+          data.content.fontSize = 3;
+
+          stylingItems.headerFontSize.change(data);
+          stylingItems.fontSize.change(data);
+          expect(data.header.fontSize).to.equal(5);
+          expect(data.content.fontSize).to.equal(5);
+        });
+
+        it('should change size to 300 when more than 300', () => {
+          data.header.fontSize = 350;
+          data.content.fontSize = 350;
+
+          stylingItems.headerFontSize.change(data);
+          stylingItems.fontSize.change(data);
+          expect(data.header.fontSize).to.equal(300);
+          expect(data.content.fontSize).to.equal(300);
+        });
+      });
+
+      describe('hoverColor/hoverFontColor.show', () => {
+        let data;
+
+        beforeEach(() => {
+          data = {
+            content: {
+              hoverEffect: true,
+            },
+          };
+        });
+
+        it('should return true when hoverEffect is true', () => {
+          expect(stylingItems.hoverColor.show(data)).to.be.true;
+          expect(stylingItems.hoverFontColor.show(data)).to.be.true;
+        });
+
+        it('should return true when hoverEffect is true', () => {
+          data.content.hoverEffect = false;
+
+          expect(stylingItems.hoverColor.show(data)).to.be.false;
+          expect(stylingItems.hoverFontColor.show(data)).to.be.false;
+        });
+      });
+    });
+  });
 
   describe('indexAdded', () => {
     it('should add index in the end of array', () => {
@@ -43,15 +181,15 @@ describe('ext', () => {
       indexRemoved(array, index);
       expect(array).to.eql([1, 0, 2]);
     });
+  });
 
-    describe('min', () => {
-      it('should return 0 when > 0', () => {
-        expect(min(3)).to.equal(0);
-      });
+  describe('min', () => {
+    it('should return 0 when > 0', () => {
+      expect(min(3)).to.equal(0);
+    });
 
-      it('should return 1 when not > 0', () => {
-        expect(min(0)).to.equal(1);
-      });
+    it('should return 1 when not > 0', () => {
+      expect(min(0)).to.equal(1);
     });
   });
 
--- src/object-properties.js
+++ src/object-properties.js
@@ -7,9 +7,9 @@ const properties = {
    * @extends {qae.HyperCubeDef}
    */
   qHyperCubeDef: {
-    /** @type {Array<qae.NxDimension>} */
+    /** @type {DimensionProperties[]} */
     qDimensions: [],
-    /** @type {Array<qae.NxMeasure>} */
+    /** @type {MeasureProperties[]} */
     qMeasures: [],
     qMode: 'S',
     /** @type {boolean} */
@@ -18,32 +18,109 @@ const properties = {
     qSuppressMissing: true,
     /** @type {Array} */
     qColumnOrder: [],
-    /** @type {Array} */
-    columnWidths: [],
   },
   /**
+   * Show title for the visualization
    * @type {boolean=}
    */
   showTitles: true,
   /**
+   * Visualization title
    * @type {string=}
    */
   title: '',
   /**
+   * Visualization subtitle
    * @type {string=}
    */
   subtitle: '',
   /**
+   * Visualization footnote
    * @type {string=}
    */
   footnote: '',
   /**
-   * @type {object}
+   * Holds general styling
+   * @type {Styling[]}
    */
-  totals: {
-    /** @type {boolean} */
-    show: false,
-  },
+  components: [],
 };
+
+/**
+ * Extends `NxDimension`, see Engine API: `NxDimension`.
+ * @typedef {object} DimensionProperties
+ * @extends NxDimension
+ * @property {InlineDimensionDef} qDef
+ * @property {AttributeExpressionProperties[]} qAttributeExpressions
+ */
+
+/**
+ * Extends `NxMeasure`, see Engine API: `NxMeasure`.
+ * @typedef {object} MeasureProperties
+ * @extends NxMeasure
+ * @property {NxInlineMeasureDef} qDef
+ * @property {AttributeExpressionProperties[]} qAttributeExpressions
+ */
+
+/**
+ * Extends `NxInlineDimensionDef`, see Engine API: `NxInlineDimensionDef`.
+ * @typedef {object} InlineDimensionDef
+ * @extends NxInlineDimensionDef
+ * @property {TextAlign=} textAlign
+ */
+
+/**
+ * Extends `NxInlineMeasureDef`, see Engine API: `NxInlineMeasureDef`.
+ * @typedef {object} InlineMeasureDef
+ * @extends NxInlineMeasureDef
+ * @property {TextAlign=} textAlign
+ */
+
+/**
+ * Extends `NxAttrExprDef`, see Engine API: `NxAttrExprDef`. Column specific styling overrides general styling, that is defined in `components`
+ * @typedef {object} AttributeExpressionProperties
+ * @extends NxAttrExprDef - expression resolving into a valid color
+ * @property {('cellForegroundColor'|'cellBackgroundColor')} id - spefifying what the color applies to
+ */
+
+/**
+ * Holds text aligmnet for a specific column
+ * @typedef {object} TextAlign
+ * @extends NxInlineDimensionDef
+ * @property {boolean} auto - If true, sets the alignment based on the type of column (left for dimension, right for measure)
+ * @property {('left'|'center'|'right')} align - Is used (and mandatoty) if `auto` is false
+ */
+
+/**
+ * General styling for all columns. Split up into header and content (body) styling. If any property is not set, the theme is used for that property. If nothing is set in the theme, default values specific for each property is used
+ * @typedef {object} Styling
+ * @property {string} key - This should be set to `theme`
+ * @property {ContentStyling=} content
+ * @property {HeaderStyling=} header
+ */
+
+/**
+ * Holds properties for font size, font color and hover styling
+ * @typedef {object} ContentStyling
+ * @property {number=} fontSize - Defualts to `14` if nothing is set here or in theme
+ * @property {PaletteColor=} fontColor - Defaults to `#404040` if nothing is set here or in theme
+ * @property {boolean=} hoverEffect - Toggles hover effect
+ * @property {PaletteColor=} hoverColor - Background hover color. Uses `#f4f4f4` if no hover colors are set, is transparent if only `hoverFontColor` is set
+ * @property {PaletteColor=} hoverFontColor - When only `hoverColor` is set, this is adjusted to either `#f4f4f4` or `#ffffff` for optimal contrast
+ */
+
+/**
+ * Holds properties for font size and color
+ * @typedef {object} HeaderStyling
+ * @property {number=} fontSize - Defualts to `14` if nothing is set here or in theme
+ * @property {PaletteColor=} fontColor - Defualts to `#404040` if nothing is set here or in theme
+ */
+
+/**
+ * Color information structure. Holds the actual color and index in palette.
+ * @typedef {object} PaletteColor
+ * @property {string} color - Color as hex string (mandatory if index: -1)
+ * @property {number} index - Index in palette
+ */
 
 export default properties;
