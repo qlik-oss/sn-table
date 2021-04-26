4a6012f016408c95eface5b7f0d17ac54a3e97d0
Li Kang <kkllpaul7766@gmail.com>
2021-04-26 11:31:42 +0200

    fix unit testing

--- src/table/__tests__/TableBodyWrapper.spec.jsx
+++ src/table/__tests__/TableBodyWrapper.spec.jsx
@@ -22,6 +22,7 @@ describe('<TableBodyWrapper />', async () => {
   let selectionsAPI;
   let layout;
   let theme;
+  let rootElement;
 
   beforeEach(async () => {
     sandbox.replace(selectionsUtils, 'addSelectionListeners', () => {});
@@ -34,6 +35,9 @@ describe('<TableBodyWrapper />', async () => {
       name: () => {},
     };
     layout = {};
+    rootElement = {
+      clientHeight: () => {},
+    };
   });
 
   afterEach(() => {
@@ -56,6 +60,7 @@ describe('<TableBodyWrapper />', async () => {
         selectionsAPI={selectionsAPI}
         layout={layout}
         theme={theme}
+        rootElement={rootElement}
       />
     );
 
@@ -76,6 +81,7 @@ describe('<TableBodyWrapper />', async () => {
         selectionsAPI={selectionsAPI}
         theme={theme}
         layout={layout}
+        rootElement={rootElement}
       />
     );
     fireEvent.keyDown(queryByText(tableData.rows[0]['id-0'].qText));
@@ -93,6 +99,7 @@ describe('<TableBodyWrapper />', async () => {
         selectionsAPI={selectionsAPI}
         theme={theme}
         layout={layout}
+        rootElement={rootElement}
       />
     );
     fireEvent.mouseDown(queryByText(tableData.rows[0]['id-0'].qText));
