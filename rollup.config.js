d19d3c552d2cabe70c63ff69a39fd4af85a315dc
Max Gefvert <max.gefvert@qlik.com>
2021-09-09 11:59:06 +0200

    fix: cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,7 @@ import postcss from 'rollup-plugin-postcss';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import image from '@rollup/plugin-image';
-import json from "@rollup/plugin-json";
-
+import json from '@rollup/plugin-json';
 import visualizer from 'rollup-plugin-visualizer';
 import { terser } from 'rollup-plugin-terser';
 import path from 'path';
@@ -14,14 +13,6 @@ import path from 'path';
 import jsxPlugin from '@babel/plugin-transform-react-jsx';
 import classProps from '@babel/plugin-proposal-class-properties';
 import chaining from '@babel/plugin-proposal-optional-chaining';
-
-import fs from 'fs';
-
-function checkInternals() {
-  // Checks if the @qlik namespace exists on disk to
-  // see if the optional dependecies can be included
-  return fs.existsSync('./node_modules/@qlik-trial');
-}
 
 const pkg = require(path.resolve(process.cwd(), 'package.json')); // eslint-disable-line  import/no-dynamic-require
 const { name, version, license, author } = pkg;
@@ -43,7 +34,7 @@ export default {
     name: moduleName,
     format: 'umd',
     exports: 'default',
-    sourcemap: true,
+    sourcemap: false,
     globals: {
       '@nebula.js/stardust': 'stardust',
     },
@@ -58,14 +49,6 @@ export default {
       'process.env.PACKAGE_VERSION': JSON.stringify(version),
       preventAssignment: true,
     }),
-    checkInternals()
-      ? replace({
-          'const __OPIONAL_THEME_DEPS__ = {};': "import { sproutBase } from '@qlik-trial/sprout-theme';",
-          delimiters: ['', ''],
-          __OPIONAL_THEME_DEPS__: 'sproutBase',
-          preventAssignment: true,
-        })
-      : {},
     external(),
     postcss(),
     babel({
