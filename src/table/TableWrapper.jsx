112c671d7960a59d791ebca406c8216a64e32c3c
Li Kang <kkllpaul7766@gmail.com>
2021-09-15 22:09:10 +0200

    feat(locale): add localization

--- src/table/TableWrapper.jsx
+++ src/table/TableWrapper.jsx
@@ -30,7 +30,7 @@ const useStyles = makeStyles({
 });
 
 export default function TableWrapper(props) {
-  const { rootElement, tableData, setPageInfo, constraints, selectionsAPI } = props;
+  const { rootElement, tableData, setPageInfo, constraints, translator, selectionsAPI } = props;
   const { size, rows, columns } = tableData;
   const [tableWidth, setTableWidth] = useState();
   const [page, setPage] = useState(0);
@@ -85,7 +85,10 @@ export default function TableWrapper(props) {
       onKeyDown={(evt) => updatePage(evt, size.qcy, page, rowsPerPage, handleChangePage, setShouldRefocus)}
     >
       <TableContainer ref={tableSection} className={classes[containerMode]}>
-        <Table stickyHeader aria-label={`showing ${rows.length + 1} rows and ${columns.length} columns`}>
+        <Table
+          stickyHeader
+          aria-label={translator.get('rows and columns', [`${rows.length + 1}`, `${columns.length}`])}
+        >
           <TableHeadWrapper {...props} focusedCellCoord={focusedCellCoord} />
           <TableBodyWrapper {...props} focusedCellCoord={focusedCellCoord} setShouldRefocus={setShouldRefocus} />
         </Table>
@@ -98,7 +101,10 @@ export default function TableWrapper(props) {
         rowsPerPage={rowsPerPage}
         page={page}
         SelectProps={{
-          inputProps: { 'aria-label': 'rows per page', 'data-testid': 'select' },
+          inputProps: {
+            'aria-label': `${translator.get('rows per page')}`,
+            'data-testid': 'select',
+          },
           native: true,
         }}
         onChangePage={handleChangePage}
@@ -112,6 +118,7 @@ TableWrapper.propTypes = {
   rootElement: PropTypes.object.isRequired,
   tableData: PropTypes.object.isRequired,
   setPageInfo: PropTypes.func.isRequired,
+  translator: PropTypes.object.isRequired,
   constraints: PropTypes.object.isRequired,
   selectionsAPI: PropTypes.object.isRequired,
 };
