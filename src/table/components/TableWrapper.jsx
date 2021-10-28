9082c9129003712b88a48d66b00e7eb499820a3c
Li Kang <kkllpaul7766@gmail.com>
2021-10-28 16:07:34 +0200

    fix: pagination select ccolor in dark mode (#370)

--- src/table/components/TableWrapper.jsx
+++ src/table/components/TableWrapper.jsx
@@ -154,6 +154,9 @@ export default function TableWrapper(props) {
             inputProps: {
               'aria-label': translator.get('SNTable.Pagination.RowsPerPage'),
               'data-testid': 'select',
+              style: {
+                color: '#404040',
+              },
               tabindex: keyboard.active ? '0' : '-1',
             },
             native: true,
