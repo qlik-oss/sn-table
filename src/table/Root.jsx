5886697657af7b91c15c179d7210641c6a82884d
Li Kang <kkllpaul7766@gmail.com>
2022-01-23 19:37:54 +0100

    refactor: mui v5 RTL with stylis-plugin-rtl

--- src/table/Root.jsx
+++ src/table/Root.jsx
@@ -1,28 +1,30 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { StyledEngineProvider, ThemeProvider } from '@mui/material/styles';
-import { create } from 'jss';
-import rtl from 'jss-rtl';
-import { StylesProvider, jssPreset } from '@mui/styles';
+import { ThemeProvider } from '@mui/material/styles';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import { StylesProvider } from '@mui/styles';
 import TableWrapper from './components/TableWrapper';
 
-// Configure JSS
-const jss = create({
-  plugins: [...jssPreset().plugins, rtl()],
-});
-
 export function render(rootElement, props) {
-  const { muiParameters } = props;
+  const { muiParameters, direction } = props;
+  const isRTL = direction === 'rtl';
+  // Create rtl cache
+  const cacheRtl = createCache({
+    key: isRTL ? 'muirtl' : 'muiltr',
+    stylisPlugins: isRTL ? [rtlPlugin] : [],
+  });
 
   ReactDOM.render(
     <React.StrictMode>
-      <StyledEngineProvider injectFirst>
-        <StylesProvider jss={jss} generateClassName={muiParameters.generateClassName}>
+      <CacheProvider value={cacheRtl}>
+        <StylesProvider generateClassName={muiParameters.generateClassName}>
           <ThemeProvider theme={muiParameters.theme}>
             <TableWrapper {...props} />
           </ThemeProvider>
         </StylesProvider>
-      </StyledEngineProvider>
+      </CacheProvider>
     </React.StrictMode>,
     rootElement
   );
