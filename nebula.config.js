34447bf508c2d38399fe6984e12be14d2939018c
Li Kang <kkllpaul7766@gmail.com>
2022-02-24 09:09:21 +0100

    fix: add missing sign for color

--- nebula.config.js
+++ nebula.config.js
@@ -42,7 +42,7 @@ module.exports = {
             },
             content: {
               fontSize: '16px',
-              color: '232323',
+              color: '#232323',
               fontFamily: 'sans-serif',
             },
           },
