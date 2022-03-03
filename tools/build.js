c7d1ec5583032f3e4ec0abe605e1891b1703f1d1
Vittorio Cellucci <vel@qlik.com>
2022-03-03 08:06:02 -0500

    fix: pin versions

--- tools/build.js
+++ tools/build.js
@@ -5,12 +5,9 @@ const yargs = require('yargs');
 const fs = require('fs-extra');
 const path = require('path');
 const build = require('@nebula.js/cli-build');
-<<<<<<< HEAD
 const sense = require('@nebula.js/cli-sense');
-=======
 const snTablePackage = require('../package.json');
 const rnSnTablePackage = require('../react-native/package.json');
->>>>>>> 99938dc (fix: sync versions)
 
 const args = yargs(process.argv.slice(2)).argv;
 const buildExt = args.ext;
@@ -18,7 +15,7 @@ const buildCore = args.core;
 const mode = args.mode || 'production';
 const watch = args.w;
 const sourcemap = mode !== 'production';
-const { carbon } = args;
+const { reactNative } = args;
 
 // cleanup old build
 fs.removeSync(path.resolve(process.cwd(), 'dist'));
@@ -45,7 +42,7 @@ if (watch) {
   buildArgs.watch = true;
 }
 
-if (carbon) {
+if (reactNative) {
   buildArgs.reactNative = true;
 }
 
@@ -55,7 +52,6 @@ const configureReactNative = () => {
   fs.removeSync(path.resolve(process.cwd(), `${reactNativeFolder}/dist`));
   rnSnTablePackage.version = snTablePackage.version;
   fs.writeFileSync(`${reactNativeFolder}/package.json`, JSON.stringify(rnSnTablePackage, null, 2));
-  console.log(snTablePackage);
 };
 
 const main = async () => {
@@ -71,7 +67,7 @@ const main = async () => {
       });
     }
   }
-  if (carbon) {
+  if (reactNative) {
     configureReactNative();
   }
   build(buildArgs);
