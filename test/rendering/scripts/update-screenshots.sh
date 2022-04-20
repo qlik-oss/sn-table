3c4482db180c72f40419f0263056f6e1b469f0bc
Li Kang <kkllpaul7766@gmail.com>
2022-04-20 20:19:09 +0200

    fix(chore): sync the playwright version between installed package and script files

--- test/rendering/scripts/update-screenshots.sh
+++ test/rendering/scripts/update-screenshots.sh
@@ -4,14 +4,15 @@
 # To know the error location in the running code
 set -e
 
-playwrightVersion=$(npm info playwright version)
+# Get the version of the installed @playwright/test
+playwrightVersion=$(npm list @playwright/test | awk '{print $2}' | cut -d "@" -f3 | sed -n 2p)
 
 # Create and start the container with the name *sn-tale-playwright*
 # using the Docker host network stack and binding your current directory
 # in the background in a “detached” mod and remove the container once it exits/stops
 # The -it instructs Docker to allocate a pseudo-TTY connected to the container’s stdin;
 # creating an interactive bash shell in the container
-docker run -d --name sn-tale-playwright  --rm --network host -v $(pwd):/work/ -w /work/ -it mcr.microsoft.com/playwright:v$playwrightVersion-focal
+docker run -d --name sn-tale-playwright --rm --network host -v $(pwd):/work/ -w /work/ -it mcr.microsoft.com/playwright:v$playwrightVersion-focal
 
 # The actual generate/update the reference screenshots is running from the machine running this shell script
 docker exec sn-tale-playwright /bin/sh -c "yarn test:rendering --update-snapshots"
