95ed5443e9141ff00b8d6797ca05319b2bb4732b
Vittorio Cellucci <vittorio.cellucci@qlik.com>
2022-04-26 03:16:03 -0400

    feat: introduce react-native-sn-table (#447)
    * feat: introduce react-native-sn-table
    
    * fix: sync versions
    
    * fix: pin versions
    
    * fix: naive merge peerDependencies
    
    * refactor: extract method renderWithCarbon
    
    * chore: resolve conflicts
    
    * fix: resolve conflicts
    
    * chore: resolve conflicts
    
    Co-authored-by: Vittorio Cellucci <vel@qlik.com>

--- tools/build.js
+++ tools/build.js
@@ -6,6 +6,8 @@ const fs = require('fs-extra');
 const path = require('path');
 const build = require('@nebula.js/cli-build');
 const sense = require('@nebula.js/cli-sense');
+const snTablePackage = require('../package.json');
+const rnSnTablePackage = require('../react-native/package.json');
 
 const args = yargs(process.argv.slice(2)).argv;
 const buildExt = args.ext;
@@ -13,6 +15,7 @@ const buildCore = args.core;
 const mode = args.mode || 'production';
 const watch = args.w;
 const sourcemap = mode !== 'production';
+const { reactNative } = args;
 
 // cleanup old build
 fs.removeSync(path.resolve(process.cwd(), 'dist'));
@@ -39,6 +42,20 @@ if (watch) {
   buildArgs.watch = true;
 }
 
+if (reactNative) {
+  buildArgs.reactNative = true;
+}
+
+// this function will sync packages.
+const configureReactNative = () => {
+  // cleanup old build
+  const reactNativeFolder = './react-native';
+  fs.removeSync(path.resolve(process.cwd(), `${reactNativeFolder}/dist`));
+  rnSnTablePackage.version = snTablePackage.version;
+  rnSnTablePackage.peerDependencies = { ...rnSnTablePackage.peerDependencies, ...snTablePackage.peerDependencies };
+  fs.writeFileSync(`${reactNativeFolder}/package.json`, JSON.stringify(rnSnTablePackage, null, 2));
+};
+
 const main = async () => {
   console.log('---> BUILDING SUPERNOVA');
   const watcher = await build(buildArgs);
@@ -52,6 +69,10 @@ const main = async () => {
       });
     }
   }
+  if (reactNative) {
+    configureReactNative();
+  }
+  build(buildArgs);
 };
 
 main();
