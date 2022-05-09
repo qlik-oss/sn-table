0c8f265624fc5455ed357a1418a3474d282ba6f7
Max Gefvert <max.gefvert@qlik.com>
2022-05-09 14:04:50 +0200

    fix: remove comments

--- src/table/components/TableWrapper.jsx
+++ src/table/components/TableWrapper.jsx
@@ -1,23 +1,18 @@
 import PropTypes from 'prop-types';
 import React, { useEffect, useState, useMemo, useRef } from 'react';
-// import ReactDOM from 'react-dom';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableContainer from '@mui/material/TableContainer';
-// import TablePagination from '@mui/material/TablePagination';
 
 import AnnounceElements from './AnnounceElements';
 import TableBodyWrapper from './TableBodyWrapper';
 import TableHeadWrapper from './TableHeadWrapper';
-// import TablePaginationActions from './TablePaginationActions';
 import PaginationBar from './PaginationBar';
 import useDidUpdateEffect from './useDidUpdateEffect';
 import { handleTableWrapperKeyDown } from '../utils/handle-key-press';
 import { updateFocus, handleResetFocus, handleFocusoutEvent } from '../utils/handle-accessibility';
 import { handleHorizontalScroll, handleNavigateTop } from '../utils/handle-scroll';
 import announcementFactory from '../utils/announcement-factory';
-
-// const Portal = ({ children, target }) => ReactDOM.createPortal(children, target);
 
 export default function TableWrapper(props) {
   const {
@@ -57,11 +52,6 @@ export default function TableWrapper(props) {
     setPageInfo({ ...pageInfo, page: pageIdx });
     announce({ keys: [['SNTable.Pagination.PageStatusReport', [pageIdx + 1, totalPages]]], politeness: 'assertive' });
   };
-
-  // const handleChangeRowsPerPage = (evt) => {
-  //   setPageInfo({ ...pageInfo, page: 0, rowsPerPage: +evt.target.value });
-  //   announce({ keys: [['SNTable.Pagination.RowsPerPageChange', evt.target.value]], politeness: 'assertive' });
-  // };
 
   useEffect(() => {
     const memoedWrapper = tableWrapperRef.current;
@@ -116,15 +106,6 @@ export default function TableWrapper(props) {
     });
   }, [rows.length, totalRowCount, totalColumnCount, page]);
 
-  // const selectProps = {
-  //   inputProps: {
-  //     'aria-label': translator.get('SNTable.Pagination.RowsPerPage'),
-  //     'data-testid': 'select',
-  //     tabIndex: !keyboard.enabled || keyboard.active ? 0 : -1,
-  //   },
-  //   native: true,
-  // };
-
   const paperStyle = {
     borderWidth: paginationNeeded ? '0px 1px 0px' : '0px',
     borderStyle: 'solid',
@@ -139,75 +120,6 @@ export default function TableWrapper(props) {
     height: constraints.active || footerContainer || paginationNeeded ? 'calc(100% - 53px)' : '100%',
     overflow: constraints.active ? 'hidden' : 'auto',
   };
-
-  // const paperTablePaginationStyle = {
-  //   display: 'flex',
-  //   justifyContent: 'flex-end',
-  //   alignItems: 'center',
-  //   paddingRight: 1,
-  //   backgroundColor: theme.table.backgroundColor,
-  //   boxShadow: 'none',
-  //   borderStyle: 'solid',
-  //   borderWidth: '0px 0px 1px 0px',
-  //   borderRadius: 0,
-  //   borderColor: theme.table.pagination.borderColor,
-  // };
-  // const tablePaginationStyle = [
-  //   constraints.active && { display: 'none' },
-  //   {
-  //     color: theme.table.pagination.color,
-  //     '& .MuiNativeSelect-icon': {
-  //       color: theme.table.pagination.iconColor,
-  //     },
-  //   },
-  // ];
-
-  // const paginationContent = (width) => {
-  //   const fixedRowsPerPage = selectionsAPI.isModal() || width < 550 || totalColumnCount > 100;
-  //   return (
-  //     <>
-  //       <TablePagination
-  //         sx={tablePaginationStyle}
-  //         rowsPerPageOptions={fixedRowsPerPage ? [rowsPerPage] : rowsPerPageOptions}
-  //         component="div"
-  //         count={totalRowCount}
-  //         rowsPerPage={rowsPerPage}
-  //         labelRowsPerPage={`${translator.get('SNTable.Pagination.RowsPerPage')}:`}
-  //         page={page}
-  //         SelectProps={selectProps}
-  //         labelDisplayedRows={({ from, to, count }) =>
-  //           width > 250 && translator.get('SNTable.Pagination.DisplayedRowsLabel', [`${from} - ${to}`, count])
-  //         }
-  //         onRowsPerPageChange={handleChangeRowsPerPage}
-  //         ActionsComponent={() => <div>{null}</div>}
-  //         onPageChange={() => {}}
-  //       />
-  //       <TablePaginationActions
-  //         theme={theme}
-  //         direction={direction}
-  //         page={page}
-  //         onPageChange={handleChangePage}
-  //         lastPageIdx={totalPages - 1}
-  //         keyboard={keyboard}
-  //         isInSelectionMode={selectionsAPI.isModal()}
-  //         tableWidth={width}
-  //         translator={translator}
-  //         constraints={constraints}
-  //       />
-  //     </>
-  //   );
-  // };
-
-  // let paginationBar;
-  // if (footerContainer) {
-  //   paginationBar = (
-  //     <Portal target={footerContainer}>
-  //       {paginationNeeded && paginationContent(footerContainer.getBoundingClientRect().width)}
-  //     </Portal>
-  //   );
-  // } else {
-  //   paginationBar = paginationNeeded && <Paper sx={paperTablePaginationStyle}>{paginationContent(rect.width)}</Paper>;
-  // }
 
   return (
     <Paper
@@ -268,7 +180,6 @@ TableWrapper.propTypes = {
   selectionsAPI: PropTypes.object.isRequired,
   theme: PropTypes.object.isRequired,
   keyboard: PropTypes.object.isRequired,
-  rect: PropTypes.object.isRequired,
   footerContainer: PropTypes.object,
   direction: PropTypes.string,
   announcer: PropTypes.func,
