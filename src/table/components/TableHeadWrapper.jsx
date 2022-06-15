8008a1b3ae1c144d16051ff722b0b6fa85926997
Li Kang <kkllpaul7766@gmail.com>
2022-06-15 16:59:42 +0200

    Update src/table/components/TableHeadWrapper.jsx
    Co-authored-by: Max Gefvert <max.gefvert@qlik.com>

--- src/table/components/TableHeadWrapper.jsx
+++ src/table/components/TableHeadWrapper.jsx
@@ -28,7 +28,7 @@ function TableHeadWrapper({
     <TableHead>
       <StyledHeadRow paginationNeeded={paginationNeeded} className="sn-table-row">
         {columns.map((column, columnIndex) => {
-          // The first cell in the head be focusable in sequential keyboard navigation,
+          // The first cell in the head is focusable in sequential keyboard navigation,
           // when nebula does not handle keyboard navigation
           const tabIndex = columnIndex === 0 && !keyboard.enabled ? 0 : -1;
           const isCurrentColumnActive = layout.qHyperCube.qEffectiveInterColumnSortOrder[0] === column.dataColIdx;
