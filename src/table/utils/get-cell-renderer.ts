6102ae0e93ce9b0c5b1562131476d054e9d9dd04
Donya MashaallahPoor <dmh@qlik.com>
2022-07-07 23:39:11 +0200

    refactor: convert getCellRenderer

--- src/table/utils/get-cell-renderer.ts
+++ src/table/utils/get-cell-renderer.ts
@@ -3,7 +3,7 @@ import withColumnStyling from '../components/withColumnStyling';
 import withSelections from '../components/withSelections';
 import withStyling from '../components/withStyling';
 
-export default function getCellRenderer(hasColumnStyling, isSelectionsEnabled) {
+export default function getCellRenderer(hasColumnStyling: boolean, isSelectionsEnabled: boolean) {
   // withStyling always runs last, applying whatever styling it gets
   let cell = withStyling(TableCell);
   if (isSelectionsEnabled) cell = withSelections(cell);
