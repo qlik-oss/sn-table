daa288554ef476f5333fd6c9f8ad194168b3cfac
rajani.varpe <rajani.varpe@qlik.com>
2022-08-03 14:38:43 +0530

    feat: completed styling panel for straight table

--- src/ext.js
+++ src/ext.js
@@ -1,4 +1,5 @@
 import Modifiers from 'qlik-modifiers';
+import { createStylingPanelDefinition, createStylingEditorDefinition } from './styling-panel-property-definition';
 
 const columnCommonHidden = {
   autoSort: {
@@ -100,124 +101,14 @@ const textAlignItems = {
 };
 
 const getStyleSettings = (env) => {
+  console.log('ENV=>', env);
   return [
     {
       type: 'items',
       items: [
-        {
-          component: 'style-editor',
-          translation: 'LayerStyleEditor.component.styling',
-          subtitle: 'LayerStyleEditor.component.styling',
-          resetBtnTranslation: 'LayerStyleEditor.component.resetAll',
-          key: 'theme',
-          ref: 'components',
-          defaultValue: [], // used by chart conversion
-          defaultValues: {
-            // used by style editor
-            key: 'theme',
-            content: {
-              fontSize: null,
-              fontColor: {
-                index: -1,
-                color: null,
-              },
-              hoverEffect: false,
-              hoverColor: {
-                index: -1,
-                color: null,
-              },
-              hoverFontColor: {
-                index: -1,
-                color: null,
-              },
-            },
-            header: {
-              fontSize: null,
-              fontColor: {
-                index: -1,
-                color: null,
-              },
-            },
-          },
-          items: {
-            chart: {
-              type: 'items',
-              items: {
-                headerFontSize: {
-                  show: true,
-                  ref: 'header.fontSize',
-                  translation: 'ThemeStyleEditor.style.headerFontSize',
-                  component: 'integer',
-                  // placeholder: () => parseInt(styleService.getStyle('header', 'fontSize'), 10),
-                  maxlength: 3,
-                  change(data) {
-                    data.header.fontSize = Math.max(5, Math.min(300, Math.floor(data.header.fontSize)));
-                  },
-                },
-                headerFontColor: {
-                  show: true,
-                  ref: 'header.fontColor',
-                  translation: 'ThemeStyleEditor.style.headerFontColor',
-                  type: 'object',
-                  component: 'color-picker',
-                  dualOutput: true,
-                },
-                fontSize: {
-                  show: true,
-                  translation: 'ThemeStyleEditor.style.cellFontSize',
-                  ref: 'content.fontSize',
-                  component: 'integer',
-                  // placeholder: () => parseInt(styleService.getStyle('content', 'fontSize'), 10),
-                  maxlength: 3,
-                  change(data) {
-                    data.content.fontSize = Math.max(5, Math.min(300, Math.floor(data.content.fontSize)));
-                  },
-                },
-                fontColor: {
-                  show: true,
-                  ref: 'content.fontColor',
-                  translation: 'ThemeStyleEditor.style.cellFontColor',
-                  type: 'object',
-                  component: 'color-picker',
-                  dualOutput: true,
-                },
-                hoverEffect: {
-                  show: true,
-                  ref: 'content.hoverEffect',
-                  translation: 'ThemeStyleEditor.style.hoverEffect',
-                  type: 'boolean',
-                  component: 'switch',
-                  options: [
-                    {
-                      value: true,
-                      translation: 'properties.on',
-                    },
-                    {
-                      value: false,
-                      translation: 'properties.off',
-                    },
-                  ],
-                },
-                hoverColor: {
-                  show: (data) => !!data.content.hoverEffect,
-                  ref: 'content.hoverColor',
-                  translation: 'ThemeStyleEditor.style.hoverStyle',
-                  type: 'object',
-                  component: 'color-picker',
-                  dualOutput: true,
-                },
-                hoverFontColor: {
-                  show: (data) => !!data.content.hoverEffect,
-                  ref: 'content.hoverFontColor',
-                  translation: 'ThemeStyleEditor.style.hoverFontStyle',
-                  type: 'object',
-                  component: 'color-picker',
-                  dualOutput: true,
-                },
-              },
-            },
-          },
-        },
+        env.flags.isEnabled('SENSECLIENT_PS_17478_STYLINGPANEL_STRAIGHT_TABLE')
+          ? createStylingPanelDefinition()
+          : createStylingEditorDefinition(),
       ],
     },
     {
---
+++ src/styling-panel-property-definition.js
@@ -0,0 +1,371 @@
+const createStylingPanelDefinition = () => {
+  const headerFontSize = {
+    label: 'Header font size',
+    component: 'panel-section',
+    items: {
+      headerFontSizeItem: {
+        component: 'items',
+        ref: 'components',
+        key: 'general',
+        items: {
+          headerFontSizeInteger: {
+            component: 'string',
+            ref: 'header.fontSize',
+            translation: 'ThemeStyleEditor.style.headerFontSize',
+            // placeholder: () => parseInt(styleService.getStyle('header', 'fontSize'), 10),
+            maxlength: 3,
+            change(data) {
+              data.header.fontSize = Math.max(5, Math.min(300, Math.floor(data.header.fontSize)));
+            },
+          },
+        },
+      },
+    },
+  };
+  const headerFontColor = {
+    label: 'Header font Color',
+    component: 'panel-section',
+    items: {
+      headerFontColorItem: {
+        component: 'items',
+        ref: 'components',
+        key: 'general',
+        items: {
+          headerFontColorWrapper: {
+            component: 'inline-wrapper',
+            items: {
+              fontColorPicker: {
+                show: true,
+                ref: 'header.fontColor',
+                translation: 'ThemeStyleEditor.style.headerFontColor',
+                type: 'object',
+                component: 'color-picker',
+                dualOutput: true,
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+  const contentFontSize = {
+    label: 'Content font size',
+    component: 'panel-section',
+    items: {
+      contentFontSizeItem: {
+        component: 'items',
+        ref: 'components',
+        key: 'general',
+        items: {
+          contentFontSizeInteger: {
+            translation: 'ThemeStyleEditor.style.cellFontSize',
+            ref: 'content.fontSize',
+            component: 'string',
+            // placeholder: () => parseInt(styleService.getStyle('content', 'fontSize'), 10),
+            maxlength: 3,
+            change(data) {
+              data.content.fontSize = Math.max(5, Math.min(300, Math.floor(data.content.fontSize)));
+            },
+          },
+        },
+      },
+    },
+  };
+  const contentFontColor = {
+    label: 'Content font color',
+    component: 'panel-section',
+    items: {
+      contentFontColorItem: {
+        component: 'items',
+        ref: 'components',
+        key: 'general',
+        items: {
+          contentFontColorWrapper: {
+            component: 'inline-wrapper',
+            items: {
+              contentFontColorPicker: {
+                ref: 'content.fontColor',
+                translation: 'ThemeStyleEditor.style.cellFontColor',
+                type: 'object',
+                component: 'color-picker',
+                dualOutput: true,
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+
+  // Uncomment to combined hover-color and hover-font-color under hoverEffect
+
+  // const hoverEffect = {
+  //   title: 'Highlight rows on hover',
+  //   component: 'panel-section',
+  //   items: {
+  //     hoverEffectItem: {
+  //       component: 'items',
+  //       ref: 'components',
+  //       key: 'general',
+  //       items: {
+  //         hoverEffectSwitch: {
+  //           show: true,
+  //           ref: 'content.hoverEffect',
+  //           translation: 'ThemeStyleEditor.style.hoverEffect',
+  //           type: 'boolean',
+  //           component: 'switch',
+  //           options: [
+  //             {
+  //               value: true,
+  //               translation: 'properties.on',
+  //             },
+  //             {
+  //               value: false,
+  //               translation: 'properties.off',
+  //             },
+  //           ],
+  //           defaultValue: false
+  //         },
+  //         hoverEffectColorItem: {
+  //           show: (data) => !!data?.content?.hoverEffect,
+  //           ref: 'content.hoverColor',
+  //           translation: 'ThemeStyleEditor.style.hoverStyle',
+  //           type: 'object',
+  //           component: 'color-picker',
+  //           dualOutput: true,
+  //         },
+  //         hoverEffectFontColorItem: {
+  //           show: (data) => !!data?.content?.hoverEffect,
+  //           ref: 'content.hoverFontColor',
+  //           translation: 'ThemeStyleEditor.style.hoverFontStyle',
+  //           type: 'object',
+  //           component: 'color-picker',
+  //           dualOutput: true,
+  //         },
+  //       },
+  //     },
+  //   },
+  // };
+
+  const hoverEffect = {
+    label: 'Highlight rows on hover',
+    component: 'panel-section',
+    items: {
+      hoverEffectItem: {
+        component: 'items',
+        ref: 'components',
+        key: 'general',
+        items: {
+          hoverEffectSwitch: {
+            ref: 'content.hoverEffect',
+            translation: 'ThemeStyleEditor.style.hoverEffect',
+            type: 'boolean',
+            component: 'switch',
+            options: [
+              {
+                value: true,
+                translation: 'properties.on',
+              },
+              {
+                value: false,
+                translation: 'properties.off',
+              },
+            ],
+            defaultValue: false,
+          },
+        },
+      },
+    },
+  };
+
+  const hoverColor = {
+    label: 'Row hover color',
+    component: 'panel-section',
+    show(data) {
+      if (data?.components[0]?.content?.hoverEffect) {
+        return true;
+      }
+      return false;
+    },
+    items: {
+      hoverEffectColorItem: {
+        component: 'items',
+        ref: 'components',
+        key: 'general',
+        items: {
+          hoverEffectColorWrapper: {
+            ref: 'content.hoverColor',
+            translation: 'ThemeStyleEditor.style.hoverStyle',
+            type: 'object',
+            component: 'color-picker',
+            dualOutput: true,
+          },
+        },
+      },
+    },
+  };
+  const hoverFontColor = {
+    label: 'Row hover font color',
+    component: 'panel-section',
+    show(data) {
+      if (data?.components[0]?.content?.hoverEffect) {
+        return true;
+      }
+      return false;
+    },
+    items: {
+      hoverEffectFontColorItem: {
+        component: 'items',
+        ref: 'components',
+        key: 'general',
+        items: {
+          hoverEffectFontColorWrapper: {
+            ref: 'content.hoverFontColor',
+            translation: 'ThemeStyleEditor.style.hoverFontStyle',
+            type: 'object',
+            component: 'color-picker',
+            dualOutput: true,
+          },
+        },
+      },
+    },
+  };
+
+  return {
+    component: 'styling-panel',
+    chartTitle: 'Object.table',
+    translation: 'LayerStyleEditor.component.styling',
+    subtitle: 'LayerStyleEditor.component.styling',
+    ref: 'components',
+    useGeneral: true,
+    key: 'theme',
+    defaultValue: [],
+    items: {
+      headerFontSizeSection: headerFontSize,
+      headerFontColorSection: headerFontColor,
+      fontSizeSection: contentFontSize,
+      contentFontColorSection: contentFontColor,
+      hoverEffectSection: hoverEffect,
+      hoverColorSection: hoverColor,
+      hoverFontColorSection: hoverFontColor,
+    },
+  };
+};
+
+const createStylingEditorDefinition = () => {
+  return {
+    component: 'style-editor',
+    translation: 'LayerStyleEditor.component.styling',
+    subtitle: 'LayerStyleEditor.component.styling',
+    resetBtnTranslation: 'LayerStyleEditor.component.resetAll',
+    key: 'theme',
+    ref: 'components',
+    defaultValue: [], // used by chart conversion
+    defaultValues: {
+      // used by style editor
+      key: 'theme',
+      content: {
+        fontSize: null,
+        fontColor: {
+          index: -1,
+          color: null,
+        },
+        hoverEffect: false,
+        hoverColor: {
+          index: -1,
+          color: null,
+        },
+        hoverFontColor: {
+          index: -1,
+          color: null,
+        },
+      },
+      header: {
+        fontSize: null,
+        fontColor: {
+          index: -1,
+          color: null,
+        },
+      },
+    },
+    items: {
+      chart: {
+        type: 'items',
+        items: {
+          headerFontSize: {
+            show: true,
+            ref: 'header.fontSize',
+            translation: 'ThemeStyleEditor.style.headerFontSize',
+            component: 'integer',
+            // placeholder: () => parseInt(styleService.getStyle('header', 'fontSize'), 10),
+            maxlength: 3,
+            change(data) {
+              data.header.fontSize = Math.max(5, Math.min(300, Math.floor(data.header.fontSize)));
+            },
+          },
+          headerFontColor: {
+            show: true,
+            ref: 'header.fontColor',
+            translation: 'ThemeStyleEditor.style.headerFontColor',
+            type: 'object',
+            component: 'color-picker',
+            dualOutput: true,
+          },
+          fontSize: {
+            show: true,
+            translation: 'ThemeStyleEditor.style.cellFontSize',
+            ref: 'content.fontSize',
+            component: 'integer',
+            // placeholder: () => parseInt(styleService.getStyle('content', 'fontSize'), 10),
+            maxlength: 3,
+            change(data) {
+              data.content.fontSize = Math.max(5, Math.min(300, Math.floor(data.content.fontSize)));
+            },
+          },
+          fontColor: {
+            show: true,
+            ref: 'content.fontColor',
+            translation: 'ThemeStyleEditor.style.cellFontColor',
+            type: 'object',
+            component: 'color-picker',
+            dualOutput: true,
+          },
+          hoverEffect: {
+            show: true,
+            ref: 'content.hoverEffect',
+            translation: 'ThemeStyleEditor.style.hoverEffect',
+            type: 'boolean',
+            component: 'switch',
+            options: [
+              {
+                value: true,
+                translation: 'properties.on',
+              },
+              {
+                value: false,
+                translation: 'properties.off',
+              },
+            ],
+          },
+          hoverColor: {
+            show: (data) => !!data.content.hoverEffect,
+            ref: 'content.hoverColor',
+            translation: 'ThemeStyleEditor.style.hoverStyle',
+            type: 'object',
+            component: 'color-picker',
+            dualOutput: true,
+          },
+          hoverFontColor: {
+            show: (data) => !!data.content.hoverEffect,
+            ref: 'content.hoverFontColor',
+            translation: 'ThemeStyleEditor.style.hoverFontStyle',
+            type: 'object',
+            component: 'color-picker',
+            dualOutput: true,
+          },
+        },
+      },
+    },
+  };
+};
+export { createStylingPanelDefinition, createStylingEditorDefinition };
