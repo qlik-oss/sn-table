c07501429ef0866d463aa695b8c6ddbc862f436a
Li Kang <kkllpaul7766@gmail.com>
2022-08-28 20:54:29 +0200

    test: update the renderting test scripts

--- test/rendering/scripts/run-rendering-test.sh
+++ test/rendering/scripts/run-rendering-test.sh
@@ -4,6 +4,15 @@
 # To know the error location in the running code
 set -e
 
+container_started=false
+
+function stopContainer() {
+  echo "stop the container"
+  [ "$container_started" == "true" ] && docker stop sn-table-playwright
+}
+
+trap stopContainer EXIT
+
 # Get the version of the installed @playwright/test
 playwrightVersion=$(npm list @playwright/test | awk '{print $2}' | cut -d "@" -f3 | sed -n 2p)
 
@@ -14,7 +23,7 @@ playwrightVersion=$(npm list @playwright/test | awk '{print $2}' | cut -d "@" -f
 # creating an interactive bash shell in the container
 docker run -d --name sn-table-playwright --rm --network host -v $(pwd):/work/ -w /work/ -it mcr.microsoft.com/playwright:v$playwrightVersion-focal
 
+container_started=true
+
 # The actual rendering test is running from the machine running this shell script
 docker exec sn-table-playwright /bin/sh -c "yarn test:rendering"
-
-docker stop sn-table-playwright
