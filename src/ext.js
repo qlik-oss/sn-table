849b99f7302331bb7286ec731739e11c4dc4fd61
rajani.varpe <rajani.varpe@qlik.com>
2022-08-29 15:02:08 +0530

    feat: removed FF for sn-table and added new definition directly in ext.js

--- src/ext.js
+++ src/ext.js
@@ -1,5 +1,4 @@
 import Modifiers from 'qlik-modifiers';
-import { stylingPanelDefinition, stylingEditorDefinition } from './styling-panel-property-definition';
 
 const columnCommonHidden = {
   autoSort: {
@@ -105,9 +104,145 @@ const getStyleSettings = (env) => {
     {
       type: 'items',
       items: [
-        env?.flags?.isEnabled('SENSECLIENT_PS_17478_STYLINGPANEL_SN_TABLE_OSS')
-          ? stylingPanelDefinition
-          : stylingEditorDefinition,
+        {
+          component: 'styling-panel',
+          chartTitle: 'Object.StraightTable',
+          translation: 'LayerStyleEditor.component.styling',
+          subtitle: 'LayerStyleEditor.component.styling',
+          ref: 'components',
+          useGeneral: true,
+          key: 'theme',
+          defaultValue: [],
+          items: {
+            headerSection: {
+              translation: 'properties.Header',
+              component: 'panel-section',
+              items: {
+                headerFontItem: {
+                  component: 'items',
+                  ref: 'components',
+                  key: 'general',
+                  items: {
+                    headerFontWrapper: {
+                      component: 'inline-wrapper',
+                      items: {
+                        headerFontSize: {
+                          component: 'integer',
+                          ref: 'header.fontSize',
+                          type: 'integer',
+                          width: 9,
+                          min: 5,
+                          max: 300,
+                          // placeholder: () => parseInt(styleService.getStyle('header', 'fontSize'), 10), // Not sure about placeholder, as qlik-oss do not have theme integrated in it
+                          change(data) {
+                            data.header.fontSize = !data.header.fontSize
+                              ? data.header.fontSize
+                              : Math.max(5, Math.min(300, Math.floor(data.header.fontSize)));
+                          },
+                        },
+                        headerFontColor: {
+                          show: true,
+                          ref: 'header.fontColor',
+                          type: 'object',
+                          component: 'color-picker',
+                          dualOutput: true,
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+            contentSection: {
+              component: 'panel-section',
+              translation: 'properties.Content',
+              items: {
+                contentFontItem: {
+                  component: 'items',
+                  ref: 'components',
+                  key: 'general',
+                  items: {
+                    contentFontWrapper: {
+                      component: 'inline-wrapper',
+                      items: {
+                        contentFontSize: {
+                          component: 'integer',
+                          ref: 'content.fontSize',
+                          type: 'integer',
+                          width: 9,
+                          min: 5,
+                          max: 300,
+                          // placeholder: () => parseInt(styleService.getStyle('content', 'fontSize'), 10), // Not sure about placeholder, as qlik-oss do not have theme integrated in it
+                          change(data) {
+                            data.content.fontSize = !data.content.fontSize
+                              ? data.content.fontSize
+                              : Math.max(5, Math.min(300, Math.floor(data.content.fontSize)));
+                          },
+                        },
+                        contentFontColor: {
+                          ref: 'content.fontColor',
+                          type: 'object',
+                          component: 'color-picker',
+                          dualOutput: true,
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+            hoverEffectSection: {
+              translation: 'properties.RowHover',
+              component: 'panel-section',
+              items: {
+                hoverEffectItem: {
+                  component: 'items',
+                  ref: 'components',
+                  key: 'general',
+                  items: {
+                    hoverEffectSwitch: {
+                      ref: 'content.hoverEffect',
+                      translation: 'ThemeStyleEditor.style.hoverEffect',
+                      type: 'boolean',
+                      component: 'switch',
+                      options: [
+                        {
+                          value: true,
+                          translation: 'properties.on',
+                        },
+                        {
+                          value: false,
+                          translation: 'properties.off',
+                        },
+                      ],
+                      defaultValue: false,
+                    },
+                    hoverEffectColorItem: {
+                      show(data) {
+                        return data?.content?.hoverEffect;
+                      },
+                      ref: 'content.hoverColor',
+                      translation: 'ThemeStyleEditor.style.hoverStyle',
+                      type: 'object',
+                      component: 'color-picker',
+                      dualOutput: true,
+                    },
+                    hoverEffectFontColorItem: {
+                      show(data) {
+                        return data?.content?.hoverEffect;
+                      },
+                      ref: 'content.hoverFontColor',
+                      translation: 'ThemeStyleEditor.style.hoverFontStyle',
+                      type: 'object',
+                      component: 'color-picker',
+                      dualOutput: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
       ],
     },
     {
