7c77fd326cd7ea6b6e68cc2e0a71233911276c78
Donya MashaallahPoor <dmh@qlik.com>
2022-09-02 08:37:46 +0200

    fix: possibility of focus on totals cell when cell is in selection mode (#679)

--- src/table/utils/__tests__/handle-key-press.spec.ts
+++ src/table/utils/__tests__/handle-key-press.spec.ts
@@ -274,6 +274,17 @@ describe('handle-key-press', () => {
       expect(nextCol).toBe(0);
     });
 
+    it('should not move to the next row when the totals row is set at the bottom', () => {
+      evt.key = 'ArrowDown';
+      const allowedRows = { top: 0, bottom: 1 };
+      rowCount = 3;
+      rowIndex = 1;
+      colIndex = 0;
+      const [nextRow, nextCol] = getNextCellCoord(evt, rootElement, [rowIndex, colIndex], allowedRows);
+      expect(nextRow).toBe(1);
+      expect(nextCol).toBe(0);
+    });
+
     it('should stay the current cell when move up', () => {
       evt.key = 'ArrowUp';
       const [nextRow, nextCol] = getNextCellCoord(evt, rootElement, [rowIndex, colIndex]);
@@ -366,36 +377,36 @@ describe('handle-key-press', () => {
       expect(nextCol).toBe(1);
     });
 
-    it('should stay at the current cell when topAllowed cell is 1 and trying to move up from rowIdx 1', () => {
+    it('should stay at the current cell when allowedRows cell index is 1 and trying to move up from rowIdx 1', () => {
       evt.key = 'ArrowUp';
-      const topAllowedRow = 1;
+      const allowedRows = { top: 1, bottom: 0 };
       rowCount = 3;
       rowIndex = 1;
-      const [nextRow, nextCol] = getNextCellCoord(evt, rootElement, [rowIndex, colIndex], topAllowedRow);
+      const [nextRow, nextCol] = getNextCellCoord(evt, rootElement, [rowIndex, colIndex], allowedRows);
       expect(nextRow).toBe(1);
       expect(nextCol).toBe(0);
     });
 
-    it('should stay at the current cell when trying to move left and topAllowedRow is > 0 (i.e in selection mode', () => {
+    it('should stay at the current cell when trying to move left and allowedRows is > 0 (i.e in selection mode', () => {
       evt.key = 'ArrowLeft';
-      const topAllowedRow = 1;
+      const allowedRows = { top: 1, bottom: 0 };
       rowCount = 3;
       columnCount = 3;
       rowIndex = 1;
       colIndex = 1;
-      const [nextRow, nextCol] = getNextCellCoord(evt, rootElement, [rowIndex, colIndex], topAllowedRow);
+      const [nextRow, nextCol] = getNextCellCoord(evt, rootElement, [rowIndex, colIndex], allowedRows);
       expect(nextRow).toBe(1);
       expect(nextCol).toBe(1);
     });
 
-    it('should stay at the current cell when trying to move right and topAllowedRow is > 0 (i.e in selection mode', () => {
+    it('should stay at the current cell when trying to move right and allowedRows is > 0 (i.e in selection mode', () => {
       evt.key = 'ArrowRight';
-      const topAllowedRow = 1;
+      const allowedRows = { top: 1, bottom: 0 };
       rowCount = 3;
       columnCount = 3;
       rowIndex = 1;
       colIndex = 1;
-      const [nextRow, nextCol] = getNextCellCoord(evt, rootElement, [rowIndex, colIndex], topAllowedRow);
+      const [nextRow, nextCol] = getNextCellCoord(evt, rootElement, [rowIndex, colIndex], allowedRows);
       expect(nextRow).toBe(1);
       expect(nextCol).toBe(1);
     });
