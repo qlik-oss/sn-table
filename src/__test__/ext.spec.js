7917952393b6213bbcca63ae0db5792f5b6b0014
Max Gefvert <max.gefvert@qlik.com>
2022-09-09 12:30:27 +0200

    test: update unit tests

--- src/__test__/ext.spec.js
+++ src/__test__/ext.spec.js
@@ -76,9 +76,12 @@ describe('ext', () => {
     });
 
     describe('styling', () => {
-      const stylingItems = definition.items.settings.items.presentation.items[0].items[0].items.chart.items;
+      const { headerSection, contentSection, hoverEffectSection } =
+        definition.items.settings.items.presentation.items[0].items[0].items;
 
       describe('headerFontSize/fontSize.change', () => {
+        const headerFont = headerSection.items.headerFontItem.items.headerFontWrapper.items.headerFontSize;
+        const contentFont = contentSection.items.contentFontItem.items.contentFontWrapper.items.contentFontSize;
         let data;
 
         beforeEach(() => {
@@ -92,35 +95,36 @@ describe('ext', () => {
           };
         });
 
-        it('should not change size when in allowed value', () => {
-          stylingItems.headerFontSize.change(data);
-          stylingItems.fontSize.change(data);
+        it('should not adjust size when in allowed value', () => {
+          headerFont.change(data);
+          contentFont.change(data);
           expect(data.header.fontSize).toBe(14);
           expect(data.content.fontSize).toBe(14);
         });
 
-        it('should change size to 5 when less than 5', () => {
+        it('should adjust size to 5 when less than 5', () => {
           data.header.fontSize = 3;
           data.content.fontSize = 3;
 
-          stylingItems.headerFontSize.change(data);
-          stylingItems.fontSize.change(data);
+          headerFont.change(data);
+          contentFont.change(data);
           expect(data.header.fontSize).toBe(5);
           expect(data.content.fontSize).toBe(5);
         });
 
-        it('should change size to 300 when more than 300', () => {
+        it('should adjust size to 300 when more than 300', () => {
           data.header.fontSize = 350;
           data.content.fontSize = 350;
 
-          stylingItems.headerFontSize.change(data);
-          stylingItems.fontSize.change(data);
+          headerFont.change(data);
+          contentFont.change(data);
           expect(data.header.fontSize).toBe(300);
           expect(data.content.fontSize).toBe(300);
         });
       });
 
       describe('hoverColor/hoverFontColor.show', () => {
+        const hoverItems = hoverEffectSection.items.hoverEffectItem.items;
         let data;
 
         beforeEach(() => {
@@ -132,15 +136,15 @@ describe('ext', () => {
         });
 
         it('should return true when hoverEffect is true', () => {
-          expect(stylingItems.hoverColor.show(data)).toBe(true);
-          expect(stylingItems.hoverFontColor.show(data)).toBe(true);
+          expect(hoverItems.hoverEffectColorItem.show(data)).toBe(true);
+          expect(hoverItems.hoverEffectFontColorItem.show(data)).toBe(true);
         });
 
         it('should return false when hoverEffect is false', () => {
           data.content.hoverEffect = false;
 
-          expect(stylingItems.hoverColor.show(data)).toBe(false);
-          expect(stylingItems.hoverFontColor.show(data)).toBe(false);
+          expect(hoverItems.hoverEffectColorItem.show(data)).toBe(false);
+          expect(hoverItems.hoverEffectFontColorItem.show(data)).toBe(false);
         });
       });
     });
