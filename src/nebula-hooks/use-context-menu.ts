dd968994921671608ae3ee853786e2b44e97b0f0
Max Gefvert <max.gefvert@qlik.com>
2022-10-05 10:00:00 +0200

    fix: sanity check to avaoid crash on old nebula versions

--- src/nebula-hooks/use-context-menu.ts
+++ src/nebula-hooks/use-context-menu.ts
@@ -4,7 +4,7 @@ import { copyCellValue } from '../table/utils/accessibility-utils';
 import { Menu } from '../types';
 
 export default function useContextMenu(areBasicFeaturesEnabled: boolean) {
-  onContextMenu((menu: Menu, event: any) => {
+  onContextMenu?.((menu: Menu, event: any) => {
     areBasicFeaturesEnabled &&
       event.target &&
       menu.addItem({
