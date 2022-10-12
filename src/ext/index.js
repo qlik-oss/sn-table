5deb95d5cb815f754a53cdd440db22e47fbf60da
Max Gefvert <max.gefvert@qlik.com>
2022-10-12 13:29:21 +0200

    refactor: split up ext

---
+++ src/ext/index.js
@@ -0,0 +1,16 @@
+import getPropertyPanelDefinition from './property-panel';
+import getData from './data';
+
+export default function ext(env) {
+  return {
+    definition: getPropertyPanelDefinition(env),
+    data: getData(env),
+    support: {
+      export: true,
+      exportData: true,
+      snapshot: true,
+      viewData: false,
+      exploration: true,
+    },
+  };
+}
