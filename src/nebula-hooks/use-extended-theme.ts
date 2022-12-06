d91ee09bbfdc8bbfe1c9d1607b486d7400e81bbd
Max Gefvert <max.gefvert@qlik.com>
2022-12-06 13:49:21 +0100

    fix: comments

--- src/nebula-hooks/use-extended-theme.ts
+++ src/nebula-hooks/use-extended-theme.ts
@@ -5,9 +5,9 @@ import { ExtendedTheme, BackgroundColors } from '../types';
 
 /**
  * The colors in the table can depend on background colors set on table, object and sheet level.
- * Even though not officially supported, it is expected that css settings on object and sheet are respected
- * The priority order is: table theme > object css > object theme > sheet css
- * If the result of those is transparent, we use the sheet css color
+ * Even though not officially supported, it is expected that css settings on object and sheet are respected.
+ * The priority order is: table theme > object css > object theme
+ * If the result for object/table background is transparent, the sheet css color is used
  */
 export const getBackgroundColors = (theme: ExtendedTheme, rootElement: HTMLElement): BackgroundColors => {
   const qvInnerObject = rootElement?.closest('.qv-object .qv-inner-object');
@@ -18,7 +18,8 @@ export const getBackgroundColors = (theme: ExtendedTheme, rootElement: HTMLEleme
 
   // tableColorFromTheme is undefined if nothing is set specifically for object.straightTable.backgroundColor
   const tableColorFromTheme = theme.getStyle('', '', 'object.straightTable.backgroundColor');
-  // colorFromTheme traverses the theme tree until it finds a value for backgroundColor, is undefined if there is no value
+  // colorFromTheme traverses the theme tree until it finds a value for backgroundColor
+  // it is undefined if there is no value
   const colorFromTheme = theme.getStyle('object', 'straightTable', 'backgroundColor');
 
   const color = tableColorFromTheme || objectColorFromCSS || colorFromTheme;
