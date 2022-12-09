eab12e96fbf19e50dcbc06215366ace79fcf8841
Christofer Bengtsson <cbt@qlik.com>
2022-12-09 08:10:14 +0100

    test: fix some tests

--- src/table/components/virtualized-table/hooks/__tests__/use-infinite-scroll-data.test.ts
+++ src/table/components/virtualized-table/hooks/__tests__/use-infinite-scroll-data.test.ts
@@ -1,17 +1,14 @@
-import { act, renderHook, waitFor } from '@testing-library/react';
+import { act, renderHook, RenderHookResult, waitFor } from '@testing-library/react';
 import { Cell, Column, PageInfo, Row, TableLayout } from '../../../../../types';
 import { generateDataPages, generateLayout } from '../../../../../__test__/generate-test-data';
 import useInfiniteScrollData, { UseInfiniteScrollData } from '../use-infinite-scroll-data';
-
-interface RenderHookResult {
-  current: UseInfiniteScrollData;
-}
 
 describe('useInfiniteScrollData', () => {
   let model: EngineAPI.IGenericObject;
   let layout: TableLayout;
   let pageInfo: PageInfo;
   let columns: Column[];
+  let renderHookResult: RenderHookResult<UseInfiniteScrollData, unknown>;
 
   beforeEach(() => {
     layout = generateLayout(1, 1, 5);
@@ -36,10 +33,11 @@ describe('useInfiniteScrollData', () => {
   });
 
   test('should load initial data and update rowsInPage', async () => {
-    let result: RenderHookResult;
     await act(async () => {
-      ({ result } = renderHook(() => useInfiniteScrollData(model, layout, pageInfo, 1, 1, columns)));
+      renderHookResult = renderHook(() => useInfiniteScrollData(model, layout, pageInfo, 1, 1, columns));
     });
+
+    const { result } = renderHookResult;
 
     const expectedRows: Row[] = [
       {
@@ -61,10 +59,12 @@ describe('useInfiniteScrollData', () => {
 
   // test('should insert row data at correct index based on current page', async () => {
   //   // Load data on the second page, for test purpose only load 1 row of data
-  //   pageInfo.page = 1;
-  //   pageInfo.rowsPerPage = 1;
-  //   const { result } = renderHook(() => useInfiniteScrollData(model, layout, pageInfo, 1, 1, columns));
-  //   result.current.loadData(0, 1, 1, 1);
+  //   pageInfo = { ...pageInfo, page: 1 };
+  //   await act(async () => {
+  //     renderHookResult = renderHook(() => useInfiniteScrollData(model, layout, pageInfo, 1, 1, columns));
+  //   });
+
+  //   const { result } = renderHookResult;
 
   //   const expectedRows: Row[] = [
   //     {
@@ -84,86 +84,70 @@ describe('useInfiniteScrollData', () => {
   //   await waitFor(() => expect(result.current.rowsInPage).toEqual(expectedRows));
   // });
 
-  // test('should reset rowsInPage when pageInfo changes', async () => {
-  //   const { result, rerender } = renderHook(() => useInfiniteScrollData(model, layout, pageInfo, columns));
-  //   result.current.loadData(0, 0, 1, 1);
+  test('should reset rowsInPage when pageInfo changes', async () => {
+    await act(async () => {
+      renderHookResult = renderHook(() => useInfiniteScrollData(model, layout, pageInfo, 1, 1, columns));
+    });
 
-  //   const expectedRows: Row[] = [
-  //     {
-  //       'col-0': {
-  //         colIdx: 0,
-  //         isLastRow: false,
-  //         isSelectable: false,
-  //         pageColIdx: 0,
-  //         pageRowIdx: 0,
-  //         qText: '0',
-  //         rowIdx: 0,
-  //       } as Cell,
-  //       key: 'row-0',
-  //     },
-  //   ];
+    const { rerender } = renderHookResult;
 
-  //   await waitFor(() => expect(result.current.rowsInPage).toEqual(expectedRows));
-  // });
+    await waitFor(() => expect(model.getHyperCubeData).toHaveBeenCalledTimes(1));
 
-  // test('should reset rowsInPage when layout changes', async () => {
-  //   let result: RenderHookResult;
-  //   let rerender: (props?: unknown) => void;
+    pageInfo = { ...pageInfo, page: 1 };
+    await act(async () => {
+      rerender({ model, layout, pageInfo });
+    });
 
-  //   await act(async () => {
-  //     ({ result, rerender } = renderHook(() => useInfiniteScrollData(model, layout, pageInfo, 1, 1, columns)));
-  //   });
+    await waitFor(() => expect(model.getHyperCubeData).toHaveBeenCalledTimes(2));
+  });
 
-  //   const expectedRows: Row[] = [
-  //     {
-  //       'col-0': {
-  //         colIdx: 0,
-  //         isLastRow: false,
-  //         isSelectable: false,
-  //         pageColIdx: 0,
-  //         pageRowIdx: 0,
-  //         qText: '0',
-  //         rowIdx: 0,
-  //       } as Cell,
-  //       key: 'row-0',
-  //     },
-  //   ];
+  test('should reset rowsInPage when layout changes', async () => {
+    await act(async () => {
+      renderHookResult = renderHook(() => useInfiniteScrollData(model, layout, pageInfo, 1, 1, columns));
+    });
 
-  //   await waitFor(() => expect(result.current.rowsInPage).toEqual(expectedRows));
+    const { rerender } = renderHookResult;
 
-  //   layout = { ...layout };
-  //   rerender({ model, layout, pageInfo });
+    await waitFor(() => expect(model.getHyperCubeData).toHaveBeenCalledTimes(1));
 
-  //   await waitFor(() => expect(result.current.rowsInPage).toEqual([]));
-  // });
+    layout = { ...layout };
+    await act(async () => {
+      rerender({ model, layout, pageInfo });
+    });
+
+    await waitFor(() => expect(model.getHyperCubeData).toHaveBeenCalledTimes(2));
+  });
 
   describe('should set correct state for isSelectable', () => {
     test('when column is a dimension and not locked', async () => {
       columns = [{ isDim: true, isLocked: false } as Column];
-      let result: RenderHookResult;
       await act(async () => {
-        ({ result } = renderHook(() => useInfiniteScrollData(model, layout, pageInfo, 1, 1, columns)));
+        renderHookResult = renderHook(() => useInfiniteScrollData(model, layout, pageInfo, 1, 1, columns));
       });
+
+      const { result } = renderHookResult;
 
       await waitFor(() => expect((result.current.rowsInPage[0]['col-0'] as Cell).isSelectable).toBe(true));
     });
 
     test('when column is a dimension and is locked', async () => {
       columns = [{ isDim: true, isLocked: true } as Column];
-      let result: RenderHookResult;
       await act(async () => {
-        ({ result } = renderHook(() => useInfiniteScrollData(model, layout, pageInfo, 1, 1, columns)));
+        renderHookResult = renderHook(() => useInfiniteScrollData(model, layout, pageInfo, 1, 1, columns));
       });
+
+      const { result } = renderHookResult;
 
       await waitFor(() => expect((result.current.rowsInPage[0]['col-0'] as Cell).isSelectable).toBe(false));
     });
 
     test('when column is a measure', async () => {
       columns = [{ isDim: false, isLocked: false } as Column];
-      let result: RenderHookResult;
       await act(async () => {
-        ({ result } = renderHook(() => useInfiniteScrollData(model, layout, pageInfo, 1, 1, columns)));
+        renderHookResult = renderHook(() => useInfiniteScrollData(model, layout, pageInfo, 1, 1, columns));
       });
+
+      const { result } = renderHookResult;
 
       await waitFor(() => expect((result.current.rowsInPage[0]['col-0'] as Cell).isSelectable).toBe(false));
     });
