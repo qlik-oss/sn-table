6a44e47a553c4b30fb4d7a9723e521575869c012
Csaba Toth <96009404+tothcsa@users.noreply.github.com>
2022-12-20 12:49:00 +0100

    test: initiation of integration tests (#745)
    * test: initiation of integration tests
    
    * test: preparing for different environments
    
    * test: pr suggestions vol3 with ci run
    
    * style: fixture file is linted
    
    * chore: fix type errors
    
    * refactor: move types
    
    * test: folder structure, other pr suggestions
    
    Co-authored-by: Christofer Bengtsson <cbt@qlik.com>

---
+++ test/integration/utils/custom-error-messages.ts
@@ -0,0 +1,14 @@
+/* eslint-disable import/prefer-default-export */
+import { Collection } from '../types';
+
+function navigateWithArrowsMsg(step: Collection): string {
+  return `
+  The test is failed on:
+  - Step: ${step.step},
+  - Description: ${step.description}
+
+  You can check the difference (Expected vs. Received) here:
+  `;
+}
+
+export { navigateWithArrowsMsg };
