60dacd3f709f67167666edcf1ffefcdd5b585592
Max Gefvert <max.gefvert@qlik.com>
2023-01-16 16:10:16 +0100

    fix: undo index

--- src/index.ts
+++ src/index.ts
@@ -59,7 +59,7 @@ const renderWithCarbon = ({
 };
 
 export default function supernova(env: Galaxy) {
-  const areBasicFeaturesEnabled = true; //env.flags.isEnabled('PS_18291_SN_TABLE_BASIC_FEATURES');
+  const areBasicFeaturesEnabled = env.flags.isEnabled('PS_18291_SN_TABLE_BASIC_FEATURES');
   return {
     qae: {
       properties: { initial: properties },
