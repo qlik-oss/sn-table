2e79e2955c9b2c77e1ff752c49f082fd2c03e626
Max Gefvert <max.gefvert@qlik.com>
2023-01-24 11:17:16 +0100

    fix: use react icons

--- src/table/hooks/use-head-icons.tsx
+++ src/table/hooks/use-head-icons.tsx
@@ -1,13 +1,13 @@
 import React, { useMemo } from 'react';
-import Lock from '@qlik-trial/sprout/icons/Lock';
-import Descending from '@qlik-trial/sprout/icons/Descending';
-import Ascending from '@qlik-trial/sprout/icons/Ascending';
+import Lock from '@qlik-trial/sprout/icons/react/Lock';
+import Descending from '@qlik-trial/sprout/icons/react/Descending';
+import Ascending from '@qlik-trial/sprout/icons/react/Ascending';
 import { Column } from '../../types';
 
 const useHeadIcons = (column: Column) => {
   return useMemo(() => {
-    const sortIcon = column.sortDirection === 'A' ? <Ascending size="small" /> : <Descending size="small" />;
-    const lockIcon = column.isLocked ? <Lock size="small" data-testid="head-cell-lock-icon" /> : undefined;
+    const sortIcon = column.sortDirection === 'A' ? <Ascending height="12px" /> : <Descending height="12px" />;
+    const lockIcon = column.isLocked ? <Lock height="12px" data-testid="head-cell-lock-icon" /> : undefined;
     return column.align === 'right' ? { startIcon: sortIcon, lockIcon } : { endIcon: sortIcon, lockIcon };
   }, [column]);
 };
