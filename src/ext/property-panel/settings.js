b5ce78db129ef253cd1dec0efda131961cd07f75
Elise Eborn <fdq@qlik.com>
2023-01-25 10:06:19 +0100

    fix: change string names

--- src/ext/property-panel/settings.js
+++ src/ext/property-panel/settings.js
@@ -211,7 +211,7 @@ const getChartExploration = (env) =>
         },
         visibilityToggler: {
           component: 'buttongroup',
-          translation: 'properties.visibilityOption',
+          translation: 'properties.ChartExploration.VisibilityOption',
           ref: 'chartExploration.menuVisibility',
           options: [
             {
@@ -220,7 +220,7 @@ const getChartExploration = (env) =>
             },
             {
               value: 'minimized',
-              translation: 'properties.minimized',
+              translation: 'properties.ChartExploration.Minimized',
             },
           ],
         },
