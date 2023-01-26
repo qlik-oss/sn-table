4110e6ad8c47baac909f3fd04d720af71ac4280f
Christofer Bengtsson <cbt@qlik.com>
2023-01-26 08:22:46 +0100

    refactor: replace useState with useRef on props change

--- src/table/virtualized-table/hooks/use-on-props-change.ts
+++ src/table/virtualized-table/hooks/use-on-props-change.ts
@@ -1,12 +1,12 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 
 // Act as a wrapper for https://beta.reactjs.org/learn/you-might-not-need-an-effect#adjusting-some-state-when-a-prop-changes
 export default function useOnPropsChange<T>(callback: () => void, props: T[]) {
-  const [prev, setPrev] = useState(props);
-  const hasChanged = prev.some((prevProp, index) => prevProp !== props[index]);
+  const prev = useRef<T[]>(props);
+  const hasChanged = prev.current.some((prevProp, index) => prevProp !== props[index]);
 
   if (hasChanged) {
-    setPrev(props);
+    prev.current = props;
     callback();
   }
 }
