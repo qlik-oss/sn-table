c58c7048b3efd52a481612b0cb8518a52e0c19e7
Christofer Bengtsson <cbt@qlik.com>
2023-02-01 11:38:09 +0100

    fix: revert on props change hook changes

--- src/table/virtualized-table/hooks/use-on-props-change.ts
+++ src/table/virtualized-table/hooks/use-on-props-change.ts
@@ -1,12 +1,12 @@
-import { useRef } from 'react';
+import { useState } from 'react';
 
 // Act as a wrapper for https://beta.reactjs.org/learn/you-might-not-need-an-effect#adjusting-some-state-when-a-prop-changes
 export default function useOnPropsChange<T>(callback: () => void, props: T[]) {
-  const prev = useRef<T[]>(props);
-  const hasChanged = prev.current.some((prevProp, index) => prevProp !== props[index]);
+  const [prev, setPrev] = useState<T[]>(props);
+  const hasChanged = prev.some((prevProp, index) => prevProp !== props[index]);
 
   if (hasChanged) {
-    prev.current = props;
+    setPrev(props);
     callback();
   }
 }
