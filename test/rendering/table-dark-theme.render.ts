22086aac1677630d3729ca42bdceed18e63b3ffe
a-m-dev <a.mirzaei.dev@gmail.com>
2023-02-09 13:33:07 +0100

    test(e2e): remove dark theme e2e scenario

--- test/rendering/table-dark-theme.render.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test';
-
-import runRenderingTests from './utils/run-rendering-tests';
-import senseHorizon from './theme/sense-horizon';
-
-test.describe('sn table: rendering tests with a dark theme and the swedish', () => {
-  runRenderingTests(senseHorizon('dark'), 'dark', 'sv-SE');
-});
