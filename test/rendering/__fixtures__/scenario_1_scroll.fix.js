cbca19cfcb1757575078108e8c9d54ee82993e6b
Christofer Bengtsson <cbt@qlik.com>
2023-02-10 14:55:18 +0100

    test: add rendering tests for vt mode

---
+++ test/rendering/__fixtures__/scenario_1_scroll.fix.js
@@ -0,0 +1,497 @@
+export default () => ({
+  type: 'sn-table',
+  genericObjects: [
+    {
+      getLayout: {
+        "qInfo": {
+          "qId": "eQcrAyS",
+          "qType": "sn-table"
+        },
+        "qMeta": {
+          "privileges": [
+            "read",
+            "update",
+            "delete",
+            "exportdata"
+          ]
+        },
+        "qSelectionInfo": {},
+        "qHyperCube": {
+          "qSize": {
+            "qcx": 1,
+            "qcy": 191
+          },
+          "qDimensionInfo": [
+            {
+              "qFallbackTitle": "AsciiAlpha",
+              "qApprMaxGlyphCount": 1,
+              "qCardinal": 191,
+              "qSortIndicator": "A",
+              "qGroupFallbackTitles": [
+                "AsciiAlpha"
+              ],
+              "qGroupPos": 0,
+              "qStateCounts": {
+                "qLocked": 0,
+                "qSelected": 0,
+                "qOption": 191,
+                "qDeselected": 0,
+                "qAlternative": 0,
+                "qExcluded": 0,
+                "qSelectedExcluded": 0,
+                "qLockedExcluded": 0
+              },
+              "qTags": [],
+              "qDimensionType": "D",
+              "qGrouping": "N",
+              "qNumFormat": {
+                "qType": "U",
+                "qnDec": 0,
+                "qUseThou": 0
+              },
+              "qIsAutoFormat": true,
+              "qGroupFieldDefs": [
+                "AsciiAlpha"
+              ],
+              "qMin": "NaN",
+              "qMax": "NaN",
+              "qAttrExprInfo": [],
+              "qAttrDimInfo": [],
+              "qCardinalities": {
+                "qCardinal": 191,
+                "qHypercubeCardinal": 0,
+                "qAllValuesCardinal": -1
+              },
+              "autoSort": true,
+              "textAlign": {
+                "auto": true,
+                "align": "left"
+              },
+              "cId": "SHnxMf"
+            }
+          ],
+          "qMeasureInfo": [],
+          "qEffectiveInterColumnSortOrder": [
+            0
+          ],
+          "qGrandTotalRow": [],
+          "qDataPages": [],
+          "qPivotDataPages": [],
+          "qStackedDataPages": [],
+          "qMode": "S",
+          "qNoOfLeftDims": -1,
+          "qTreeNodesOnDim": [],
+          "qColumnOrder": [
+            0
+          ],
+          "columnWidths": [
+            -1
+          ]
+        },
+        "showTitles": true,
+        "title": "",
+        "subtitle": "",
+        "footnote": "",
+        "disableNavMenu": false,
+        "showDetails": true,
+        "showDetailsExpression": false,
+        "components": [],
+        "totals": {
+          "show": true,
+          "position": "noTotals",
+          "label": "Totals"
+        },
+        "visualization": "sn-table",
+        "version": "\"1.22.0\"",
+        "enableChartExploration": false,
+        "presentation": {
+          "usePagination": false
+        }
+      },
+      getHyperCubeData: [{
+        "qMatrix": [
+          [
+            {
+              "qText": "­",
+              "qNum": "NaN",
+              "qElemNumber": 108,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": " ",
+              "qNum": "NaN",
+              "qElemNumber": 0,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": " ",
+              "qNum": "NaN",
+              "qElemNumber": 95,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "_",
+              "qNum": "NaN",
+              "qElemNumber": 63,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "-",
+              "qNum": "NaN",
+              "qElemNumber": 13,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": ",",
+              "qNum": "NaN",
+              "qElemNumber": 12,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": ";",
+              "qNum": "NaN",
+              "qElemNumber": 27,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": ":",
+              "qNum": "NaN",
+              "qElemNumber": 26,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "!",
+              "qNum": "NaN",
+              "qElemNumber": 1,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "¡",
+              "qNum": "NaN",
+              "qElemNumber": 96,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "?",
+              "qNum": "NaN",
+              "qElemNumber": 31,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "¿",
+              "qNum": "NaN",
+              "qElemNumber": 126,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": ".",
+              "qNum": "NaN",
+              "qElemNumber": 14,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "·",
+              "qNum": "NaN",
+              "qElemNumber": 118,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "'",
+              "qNum": "NaN",
+              "qElemNumber": 7,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "\"",
+              "qNum": "NaN",
+              "qElemNumber": 2,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "«",
+              "qNum": "NaN",
+              "qElemNumber": 106,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "»",
+              "qNum": "NaN",
+              "qElemNumber": 122,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "(",
+              "qNum": "NaN",
+              "qElemNumber": 8,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": ")",
+              "qNum": "NaN",
+              "qElemNumber": 9,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "[",
+              "qNum": "NaN",
+              "qElemNumber": 59,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "]",
+              "qNum": "NaN",
+              "qElemNumber": 61,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "{",
+              "qNum": "NaN",
+              "qElemNumber": 91,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "}",
+              "qNum": "NaN",
+              "qElemNumber": 93,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "§",
+              "qNum": "NaN",
+              "qElemNumber": 102,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "¶",
+              "qNum": "NaN",
+              "qElemNumber": 117,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "@",
+              "qNum": "NaN",
+              "qElemNumber": 32,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "*",
+              "qNum": "NaN",
+              "qElemNumber": 10,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "/",
+              "qNum": "NaN",
+              "qElemNumber": 15,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "\\",
+              "qNum": "NaN",
+              "qElemNumber": 60,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "&",
+              "qNum": "NaN",
+              "qElemNumber": 6,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "#",
+              "qNum": "NaN",
+              "qElemNumber": 3,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "%",
+              "qNum": "NaN",
+              "qElemNumber": 5,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "`",
+              "qNum": "NaN",
+              "qElemNumber": 64,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "´",
+              "qNum": "NaN",
+              "qElemNumber": 115,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "^",
+              "qNum": "NaN",
+              "qElemNumber": 62,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "¯",
+              "qNum": "NaN",
+              "qElemNumber": 110,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "¨",
+              "qNum": "NaN",
+              "qElemNumber": 103,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "¸",
+              "qNum": "NaN",
+              "qElemNumber": 119,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "°",
+              "qNum": "NaN",
+              "qElemNumber": 111,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "©",
+              "qNum": "NaN",
+              "qElemNumber": 104,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "®",
+              "qNum": "NaN",
+              "qElemNumber": 109,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "+",
+              "qNum": "NaN",
+              "qElemNumber": 11,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "±",
+              "qNum": "NaN",
+              "qElemNumber": 112,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "÷",
+              "qNum": "NaN",
+              "qElemNumber": 182,
+              "qState": "O"
+            }
+          ],
+          [
+            {
+              "qText": "×",
+              "qNum": "NaN",
+              "qElemNumber": 150,
+              "qState": "O"
+            }
+          ]
+        ],
+        "qTails": [
+          {
+            "qUp": 0,
+            "qDown": 0
+          }
+        ],
+        "qArea": {
+          "qLeft": 0,
+          "qTop": 0,
+          "qWidth": 1,
+          "qHeight": 46
+        }
+      }],
+    },
+  ],
+});
