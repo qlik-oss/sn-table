6d3abf8d8df39dfdd82ac247b18082f3b79ce018
Max Gefvert <max.gefvert@qlik.com>
2023-02-16 15:16:13 +0100

    fix: only add button padding for header

--- src/table/virtualized-table/utils/get-height.ts
+++ src/table/virtualized-table/utils/get-height.ts
@@ -1,11 +1,11 @@
 import { COMMON_CELL_STYLING } from '../../styling-defaults';
 import { GeneratedStyling } from '../../types';
 import { fontSizeToRowHeight } from '../../utils/styling-utils';
-import { MIN_BODY_ROW_HEIGHT, MIN_HEADER_HEIGHT } from '../constants';
+import { MIN_BODY_ROW_HEIGHT, MIN_HEADER_HEIGHT, PADDING_TOP_BOTTOM } from '../constants';
 import { BodyStyle, Rect, Totals } from '../types';
 
 const getHeaderRowHeight = ({ fontSize = COMMON_CELL_STYLING.fontSize }: GeneratedStyling) =>
-  Math.max(MIN_HEADER_HEIGHT, fontSizeToRowHeight(fontSize));
+  Math.max(MIN_HEADER_HEIGHT, fontSizeToRowHeight(fontSize) + PADDING_TOP_BOTTOM) * 2;
 
 const getBodyRowHeight = ({ fontSize = COMMON_CELL_STYLING.fontSize }: BodyStyle) =>
   Math.max(MIN_BODY_ROW_HEIGHT, fontSizeToRowHeight(fontSize));
