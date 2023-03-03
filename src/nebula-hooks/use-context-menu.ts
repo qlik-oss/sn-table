024542840b8202265bc09e31900676b21bea242a
a-m-dev <a.mirzaei.dev@gmail.com>
2023-03-03 16:41:28 +0100

    fix: copy cell only on cells

--- src/nebula-hooks/use-context-menu.ts
+++ src/nebula-hooks/use-context-menu.ts
@@ -2,10 +2,13 @@
 import { onContextMenu } from '@nebula.js/stardust';
 import copyCellValue from '../table/utils/copy-utils';
 
+const tagNamesWhiteList = new Set(['TD', 'TH']);
+
 export default function useContextMenu(areBasicFeaturesEnabled: boolean) {
   onContextMenu?.((menu: any, event: any) => {
     areBasicFeaturesEnabled &&
       event.target &&
+      tagNamesWhiteList.has(event.target.closest('.sn-table-cell')?.tagName) &&
       menu.addItem({
         translation: 'contextMenu.copyCellValue',
         icon: 'lui-icon lui-icon--copy',
