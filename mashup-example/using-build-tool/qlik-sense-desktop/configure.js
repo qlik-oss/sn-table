146814e0eea15cb035518ab929e1c88483027711
LiKang6688 <kkllpaul7766@gmail.com>
2023-03-23 21:47:51 +0100

    chore: ignore mashup-example

--- mashup-example/using-build-tool/qlik-sense-desktop/configure.js
+++ mashup-example/using-build-tool/qlik-sense-desktop/configure.js
@@ -1,5 +1,3 @@
-/* eslint-disable import/no-unresolved */
-/* eslint-disable import/extensions */
 import { embed } from '@nebula.js/stardust';
 // import table from '@nebula.js/sn-table';
 // To load local sn-table extension
