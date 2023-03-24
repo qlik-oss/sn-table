a2f371c663540350c8b7c60228fdad123b5fe764
Christofer Bengtsson <cbt@qlik.com>
2023-03-24 14:23:40 +0100

    fix: head cell width

--- src/table/virtualized-table/utils/cell-width-utils.ts
+++ src/table/virtualized-table/utils/cell-width-utils.ts
@@ -1,20 +1,24 @@
 import { Column } from '../../../types';
 import { BORDER_WIDTH, PADDING_LEFT_RIGHT } from '../constants';
 
-// Actual padding is 12px * 2, but as this is used when measuring text an extra pixels are added as the measurement is not perfect
-const AMPLIFIED_PADDING = PADDING_LEFT_RIGHT + 2;
+const HEAD_CELL_PADDING = 4 * 2;
+const HEAD_BUTTON_PADDING = 8 * 2;
 const SORT_ICON_WIDTH = 18;
 const LOCK_ICON_WIDTH = 24;
-const MENU_BUTTON_WIDTH = 28;
+const MENU_BUTTON_WIDTH = 24;
+const GRID_GAP = 4;
 
-export const subtractCellPaddingAndBorder = (width: number) => width - AMPLIFIED_PADDING * 2 - BORDER_WIDTH;
+export const getAdjustedCellWidth = (width: number) => width - PADDING_LEFT_RIGHT * 2 - BORDER_WIDTH;
 
-export const subtractCellPaddingIconsAndBorder = (width: number, column: Column) => {
-  let newWidth = width - AMPLIFIED_PADDING * 2 - BORDER_WIDTH - SORT_ICON_WIDTH;
+export const getAdjustedHeadCellWidth = (width: number, column: Column) => {
+  let newWidth = width;
+  newWidth -= HEAD_CELL_PADDING;
+  newWidth -= HEAD_BUTTON_PADDING;
+  newWidth -= BORDER_WIDTH;
+  newWidth -= SORT_ICON_WIDTH;
+  newWidth -= MENU_BUTTON_WIDTH;
+  newWidth -= GRID_GAP;
   newWidth -= column.isLocked ? LOCK_ICON_WIDTH : 0;
-  newWidth -= column.isDim ? MENU_BUTTON_WIDTH : 0;
 
   return newWidth;
 };
-
-export const appendCellPaddingAndBorder = (width: number) => width + AMPLIFIED_PADDING * 2 + BORDER_WIDTH;
