e8f32b092903a0d93ce40264c291f19c0a5d894f
a-m-dev <a.mirzaei.dev@gmail.com>
2023-03-30 13:37:16 +0200

    chore: upd

--- src/handle-data.ts
+++ src/handle-data.ts
@@ -52,7 +52,8 @@ export function getTotalPosition(layout: TableLayout) {
  */
 export function getTotalInfo(layout: TableLayout, colIdx: number, pageColIdx: number, numDims: number) {
   if (colIdx >= numDims) return layout.qHyperCube.qGrandTotalRow[colIdx - numDims]?.qText ?? '';
-  return pageColIdx === 0 ? layout.totals.label ?? '' : '';
+  if (pageColIdx === 0) return layout.totals.label ?? '';
+  return '';
 }
 
 /**
