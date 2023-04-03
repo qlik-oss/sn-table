417729fafe50e2902a2c78e22b5e3c09f05ca5c4
Christofer Bengtsson <cbt@qlik.com>
2023-04-03 09:58:14 +0200

    perf: measure text (#961)

--- src/table/virtualized-table/hooks/use-measure-text.ts
+++ src/table/virtualized-table/hooks/use-measure-text.ts
@@ -2,13 +2,60 @@ import { useMemo } from 'react';
 import { memoize } from 'qlik-chart-modules';
 import { MAX_NBR_LINES_OF_TEXT } from '../constants';
 
+type EstimateLineCountProps = {
+  text: string;
+  maxWidth: number;
+  isNumeric?: boolean;
+};
+
+export type EstimateLineCount = ({ text, maxWidth, isNumeric }: EstimateLineCountProps) => number;
+
 export interface MeasureTextHook {
   estimateWidth: (length: number) => number;
   measureText: (text: string) => number;
-  estimateLineCount: (text: string, maxWidth: number, isNumeric?: boolean) => number;
+  estimateLineCount: EstimateLineCount;
 }
 
 const MAGIC_DEFAULT_CHAR = 'N';
+
+function getNextLine(text: string, maxWidth: number, fixedMeasureText: (text: string) => number) {
+  let left = 0;
+  let right = text.length;
+
+  if (text.length <= 1) {
+    return null;
+  }
+
+  if (fixedMeasureText(text) <= maxWidth) {
+    return null;
+  }
+
+  while (left <= right) {
+    const m = Math.floor((left + right) / 2);
+    const chunk = text.slice(0, m);
+    const prevChunk = text.slice(0, m - 1);
+    const width = fixedMeasureText(chunk);
+    const prevWidth = fixedMeasureText(prevChunk);
+
+    if (width > maxWidth && prevWidth <= maxWidth) {
+      return text.slice(m - 1) || null;
+    }
+
+    if (width === maxWidth) {
+      return text.slice(m) || null;
+    }
+
+    if (width < maxWidth) {
+      // Search right
+      left = m + 1;
+    } else {
+      // Search left
+      right = m;
+    }
+  }
+
+  return null;
+}
 
 export default function useMeasureText(
   fontSize: string | undefined,
@@ -21,34 +68,30 @@ export default function useMeasureText(
 
     const memoizedMeasureText = memoize(context.measureText.bind(context)) as (text: string) => TextMetrics;
 
+    const fixedMeasureText = (text: string) => +memoizedMeasureText(text).width.toFixed(2);
+
+    const toKey = ({ text, maxWidth, isNumeric }: EstimateLineCountProps) => `${text}-${maxWidth}-${isNumeric}`;
+
+    const memoizedEstimateLineCount = memoize(({ text, maxWidth, isNumeric = false }: EstimateLineCountProps) => {
+      if (isNumeric) {
+        return 1;
+      }
+
+      let lineCount = 0;
+      let nextLine: string | null = text;
+
+      do {
+        lineCount += 1;
+        nextLine = getNextLine(nextLine, maxWidth, fixedMeasureText);
+      } while (nextLine !== null && lineCount < MAX_NBR_LINES_OF_TEXT);
+
+      return lineCount;
+    }, toKey) as EstimateLineCount;
+
     return {
       measureText: (text) => memoizedMeasureText(text).width,
       estimateWidth: (length: number) => memoizedMeasureText(MAGIC_DEFAULT_CHAR).width * length,
-      estimateLineCount: (text: string, maxWidth: number, isNumeric = false) => {
-        let lineCount = 1;
-        let startIndex = 0;
-
-        if (isNumeric) {
-          return lineCount;
-        }
-
-        for (let index = 1; index <= text.length; index++) {
-          const chars = text.slice(startIndex, index);
-          const width = +memoizedMeasureText(chars).width.toFixed(2);
-
-          if (width > maxWidth) {
-            lineCount += 1;
-            startIndex = index - 1;
-          }
-
-          // No need to measure more text if max nbr of lines have already been reached
-          if (lineCount >= MAX_NBR_LINES_OF_TEXT) {
-            return MAX_NBR_LINES_OF_TEXT;
-          }
-        }
-
-        return lineCount;
-      },
+      estimateLineCount: memoizedEstimateLineCount,
     };
   }, [fontSize, fontFamily, boldText]);
 
