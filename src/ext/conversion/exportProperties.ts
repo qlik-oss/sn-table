2d801b3ed010cf1d4a4ab160b67ace56e96c5899
Li Kang <kkllpaul7766@gmail.com>
2023-04-06 11:22:55 +0200

    feat: mapping column widths for chart conversion (#975)

--- src/ext/conversion/exportProperties.ts
+++ src/ext/conversion/exportProperties.ts
@@ -1,4 +1,5 @@
 import conversion from 'qlik-object-conversion';
+import { setValue } from 'qlik-chart-modules';
 
 import { ExportFormat, PropTree } from '../../types';
 
@@ -7,6 +8,31 @@ const exportProperties = (propertyTree: PropTree, hyperCubePath: string): Export
     propertyTree,
     hyperCubePath,
   });
+
+  const {
+    qHyperCubeDef: { qColumnOrder, qDimensions, qMeasures },
+  } = expFormat.properties;
+
+  const numDims = qDimensions.length;
+  const columnsLength = numDims + qMeasures.length;
+  const columnOrder = qColumnOrder?.length === columnsLength ? qColumnOrder : Array.from(Array(columnsLength).keys());
+
+  const columnWidths = columnOrder.map((colIdx: number) => {
+    const isDim = colIdx < numDims;
+    const info = isDim ? qDimensions[colIdx] : qMeasures[colIdx - numDims];
+
+    const { columnWidth } = info.qDef;
+
+    // For converting "sn-table to table", columnWidth is -1 unless the width type is pixels.
+    return columnWidth?.type === 'pixels' ? columnWidth.pixels : -1;
+  });
+
+  const qHyperCubeDef = {
+    ...expFormat.properties.qHyperCubeDef,
+    columnWidths,
+  };
+  setValue(expFormat, 'properties.qHyperCubeDef', qHyperCubeDef);
+  conversion.quarantineProperty(expFormat.properties, 'qHyperCubeDef.columnWidths', 'straightTableColumnWidths');
 
   return expFormat;
 };
