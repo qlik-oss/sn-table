de489341bf3520458d33668280ee6845bf695af5
quanho <quan.ho@qlik.com>
2023-06-01 13:54:02 +0200

    refactor: clean up after rebase

--- src/ext/property-panel/settings.js
+++ src/ext/property-panel/settings.js
@@ -248,9 +248,6 @@ const getChartExploration = (env) =>
           ],
         },
       },
-      // straight table that has chart exploration enabled and is placed inside a container
-      // should not show Chart exploration in the property panel
-      show: (itemData) => !itemData.insideContainer,
     },
   };
 
