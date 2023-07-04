dcc155318cebeb357ff68ee0f6b1c0410457933e
Sharlene Aminullah <sharlene.aminullah@qlik.com>
2023-07-04 15:03:00 +0200

    feat: removes empty space by adding more rows at bottom

--- src/table/hooks/use-snapshot.ts
+++ src/table/hooks/use-snapshot.ts
@@ -3,6 +3,7 @@ import { onTakeSnapshot, useImperativeHandle, stardust } from '@nebula.js/stardu
 import type { TableLayout, ViewService, SnapshotLayout, HyperCube } from '../../types';
 import { findPaginationVisibleRows, findVirtualizedVisibleRows } from '../utils/find-visible-rows';
 import { getTotalPosition } from '../../handle-data';
+import { initialPageInfo } from '../../nebula-hooks/use-pagination-table';
 
 interface UseSnapshotProps {
   layout: TableLayout;
@@ -21,10 +22,21 @@ const useSnapshot = ({ layout, viewService, model, rootElement, contentRect }: U
         rootElement,
         totalsPosition
       );
+
+      const totalRowCount = layout.qHyperCube.qSize.qcy;
+      const paginationNeeded = totalRowCount > 10;
+      const rowsPerPage = viewService.rowsPerPage || initialPageInfo.rowsPerPage;
+      const rowsRendered = visibleRowEndIndex - visibleRowStartIndex + 1;
+      const getVisibleHeight = () => {
+        return paginationNeeded && rowsRendered < rowsPerPage
+          ? visibleRowEndIndex - visibleRowStartIndex + 4 // Temp:is considered to avoid ratio change to remove empty space at the bottom
+          : rowsRendered;
+      };
+
       return {
         scrollLeft: viewService.scrollLeft,
         visibleTop: viewService.qTop + visibleRowStartIndex,
-        visibleHeight: visibleRowEndIndex < 0 ? 0 : visibleRowEndIndex - visibleRowStartIndex + 1,
+        visibleHeight: visibleRowEndIndex < 0 ? 0 : getVisibleHeight(),
         rowsPerPage: viewService.rowsPerPage,
         page: viewService.page,
       };
