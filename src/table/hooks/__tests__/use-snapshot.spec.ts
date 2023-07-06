d8204cf0233b2fcf07adc5168d26cbaf5bd3d669
Sharlene Aminullah <sharlene.aminullah@qlik.com>
2023-07-06 21:39:33 +0200

    feat: add more unit test

--- src/table/hooks/__tests__/use-snapshot.spec.ts
+++ src/table/hooks/__tests__/use-snapshot.spec.ts
@@ -10,8 +10,7 @@ describe('use-snapshot', () => {
   const dimensionCount = 2;
   const measureCount = 1;
   let rowCount = 100;
-
-  it('should return the minimum value for the row number', async () => {
+  it('should check if qcy is use', async () => {
     visibleRowStartIndex = 0;
     visibleRowEndIndex = 111;
     layout = generateLayout(dimensionCount, measureCount, rowCount);
@@ -28,34 +27,15 @@ describe('use-snapshot', () => {
     expect(result).toBe(rowCount);
   });
 
-  it('should  pick up row count 22', async () => {
+  it('should  check if rowsPerPage is used', async () => {
     visibleRowStartIndex = 0;
-    visibleRowEndIndex = 21;
+    visibleRowEndIndex = 13;
     rowCount = 22;
     layout = generateLayout(dimensionCount, measureCount, rowCount);
     viewService = {
       scrollTopRatio: 1,
       visibleTop: 0,
-      visibleHeight: 22,
-      page: 0,
-      rowsPerPage: 100,
-      qTop: 0,
-      qHeight: 100,
-    };
-
-    const result = getVisibleHeight(visibleRowEndIndex, visibleRowStartIndex, layout, viewService);
-    expect(result).toBe(rowCount);
-  });
-
-  it('should  pick up row count 10', async () => {
-    visibleRowStartIndex = 0;
-    visibleRowEndIndex = 27;
-    rowCount = 10;
-    layout = generateLayout(dimensionCount, measureCount, rowCount);
-    viewService = {
-      scrollTopRatio: 1,
-      visibleTop: 0,
-      visibleHeight: 10,
+      visibleHeight: 14,
       page: 0,
       rowsPerPage: 10,
       qTop: 0,
@@ -63,6 +43,42 @@ describe('use-snapshot', () => {
     };
 
     const result = getVisibleHeight(visibleRowEndIndex, visibleRowStartIndex, layout, viewService);
-    expect(result).toBe(rowCount);
+    expect(result).toBe(viewService.rowsPerPage);
+  });
+
+  it('should  add extra rows when row count is larger than rowsPerPage', async () => {
+    visibleRowStartIndex = 0;
+    visibleRowEndIndex = 5;
+    rowCount = 11;
+    layout = generateLayout(dimensionCount, measureCount, rowCount);
+    viewService = {
+      scrollTopRatio: 1,
+      visibleTop: 0,
+      visibleHeight: 6,
+      page: 0,
+      rowsPerPage: 10,
+      qTop: 0,
+      qHeight: 100,
+    };
+
+    const result = getVisibleHeight(visibleRowEndIndex, visibleRowStartIndex, layout, viewService);
+    expect(result).toBe(9);
+  });
+
+  it('should return 0 when visibleRowEndIndex < 0 ', async () => {
+    visibleRowStartIndex = 0;
+    visibleRowEndIndex = -1;
+    rowCount = 10;
+    layout = generateLayout(dimensionCount, measureCount, rowCount);
+    viewService = {
+      visibleTop: 0,
+      visibleHeight: 10,
+      rowsPerPage: 10,
+      qTop: 0,
+      qHeight: 100,
+    };
+
+    const result = getVisibleHeight(visibleRowEndIndex, visibleRowStartIndex, layout, viewService);
+    expect(result).toBe(0);
   });
 });
