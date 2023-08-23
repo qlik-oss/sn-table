f33a87caabba6456b10e097ccd26e2471c2045f4
Sharlene Aminullah <sharlene.aminullah@qlik.com>
2023-08-23 14:54:02 +0200

    fix: unnecessary isprinting check

--- src/nebula-hooks/use-pagination-table.ts
+++ src/nebula-hooks/use-pagination-table.ts
@@ -62,7 +62,7 @@ const usePaginationTable = ({
 }: UsePaginationTable) => {
   const { direction, footerContainer } = useOptions() as UseOptions;
   const isPrintingMode = isPrinting(layout, viewService);
-  const shouldRender = layout.usePagination !== false || isPrintingMode;
+  const shouldRender = layout.usePagination !== false;
   const announce = useAnnounceAndTranslations(rootElement, translator);
   const tmpPageInfo = isPrintingMode
     ? {
