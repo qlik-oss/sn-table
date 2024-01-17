ef1c535ad76c0321bedcdc5a626b50f37d4c5495
Christofer Bengtsson <cbt@qlik.com>
2024-01-17 10:45:59 +0100

    chore: update deps

--- jest/setup.ts
+++ jest/setup.ts
@@ -2,11 +2,11 @@ import "@testing-library/jest-dom";
 
 beforeEach(() => {
   const context = {
-    measureText(text) {
+    measureText(text: string | undefined) {
       return {
         width: text?.length ?? 0,
       };
     },
-  };
+  } as CanvasRenderingContext2D;
   jest.spyOn(HTMLCanvasElement.prototype, "getContext").mockReturnValue(context);
 });
